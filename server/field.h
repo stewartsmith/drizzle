0feed481c8bce252e4e0bc52b99d0f60d8c9dc43
Toru Maesaka <dev@torum.net>
2008-07-18 18:08:53 +0900

    ripped out DOUBLE and moved to field/

--- server/field.h
+++ server/field.h
@@ -24,6 +24,8 @@
 #endif
 
 #define DATETIME_DEC                     6
+#define DOUBLE_TO_STRING_CONVERSION_BUFFER_SIZE FLOATING_POINT_BUFFER
+
 const uint32 max_field_size= (uint32) 4294967295U;
 
 class Send_field;
@@ -918,45 +920,6 @@ public:
   }
 };
 
-class Field_double :public Field_real {
-public:
-  Field_double(uchar *ptr_arg, uint32 len_arg, uchar *null_ptr_arg,
-	       uchar null_bit_arg,
-	       enum utype unireg_check_arg, const char *field_name_arg,
-	       uint8 dec_arg,bool zero_arg,bool unsigned_arg)
-    :Field_real(ptr_arg, len_arg, null_ptr_arg, null_bit_arg,
-                unireg_check_arg, field_name_arg,
-                dec_arg, zero_arg, unsigned_arg)
-    {}
-  Field_double(uint32 len_arg, bool maybe_null_arg, const char *field_name_arg,
-	       uint8 dec_arg)
-    :Field_real((uchar*) 0, len_arg, maybe_null_arg ? (uchar*) "" : 0, (uint) 0,
-                NONE, field_name_arg, dec_arg, 0, 0)
-    {}
-  Field_double(uint32 len_arg, bool maybe_null_arg, const char *field_name_arg,
-	       uint8 dec_arg, my_bool not_fixed_arg)
-    :Field_real((uchar*) 0, len_arg, maybe_null_arg ? (uchar*) "" : 0, (uint) 0,
-                NONE, field_name_arg, dec_arg, 0, 0)
-    {not_fixed= not_fixed_arg; }
-  enum_field_types type() const { return MYSQL_TYPE_DOUBLE;}
-  enum ha_base_keytype key_type() const { return HA_KEYTYPE_DOUBLE; }
-  int  store(const char *to,uint length,CHARSET_INFO *charset);
-  int  store(double nr);
-  int  store(int64_t nr, bool unsigned_val);
-  int reset(void) { bzero(ptr,sizeof(double)); return 0; }
-  double val_real(void);
-  int64_t val_int(void);
-  String *val_str(String*,String *);
-  bool send_binary(Protocol *protocol);
-  int cmp(const uchar *,const uchar *);
-  void sort_string(uchar *buff,uint length);
-  uint32 pack_length() const { return sizeof(double); }
-  uint row_pack_length() { return pack_length(); }
-  void sql_type(String &str) const;
-private:
-  int do_save_field_metadata(uchar *first_byte);
-};
-
 
 class Field_enum :public Field_str {
 protected:
@@ -1168,6 +1131,7 @@ check_string_copy_error(Field_str *field,
 #include "field/year.h"
 #include "field/date.h"
 #include "field/decimal.h"
+#include "field/double.h"
 #include "field/timetype.h"
 #include "field/timestamp.h"
 #include "field/datetime.h"
