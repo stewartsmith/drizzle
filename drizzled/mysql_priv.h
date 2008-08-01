86411ac6e0ff7e8c1ffe3e416595720f21adc14a
Brian Aker <brian@tangent.org>
2008-08-01 13:40:12 -0700

    Removed dead structures for I_S which are gone.

--- drizzled/mysql_priv.h
+++ drizzled/mysql_priv.h
@@ -1375,7 +1375,6 @@ extern bool opt_log;
 extern bool opt_slow_log;
 extern ulong log_output_options;
 extern bool opt_log_queries_not_using_indexes;
-extern bool opt_disable_networking, opt_skip_show_db;
 extern bool opt_character_set_client_handshake;
 extern bool volatile abort_loop, shutdown_in_progress;
 extern uint volatile thread_count, thread_running, global_read_lock;
