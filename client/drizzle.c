e90a285925b7aa437a20ca951163d9e788ca2d08
Monty Taylor <monty@inaugust.com>
2008-08-01 17:45:47 -0700

    Changed gettext() to _() in drizzle.c.

--- client/drizzle.c
+++ client/drizzle.c
@@ -1123,7 +1123,7 @@ int main(int argc,char *argv[])
   window_resize(0);
 #endif
 
-  put_info(gettext("Welcome to the Drizzle client..  Commands end with ; or \\g."),
+  put_info(_("Welcome to the Drizzle client..  Commands end with ; or \\g."),
            INFO_INFO,0,0);
 
   glob_buffer= (DYNAMIC_STRING *)my_malloc(sizeof(DYNAMIC_STRING), MYF(0));
@@ -1131,7 +1131,7 @@ int main(int argc,char *argv[])
 
   /* this is a slight abuse of the DYNAMIC_STRING interface. deal. */
   sprintf(glob_buffer->str,
-          gettext("Your Drizzle connection id is %u\nServer version: %s\n"),
+          _("Your Drizzle connection id is %u\nServer version: %s\n"),
           drizzle_thread_id(&drizzle), server_version_string(&drizzle));
   put_info(glob_buffer->str, INFO_INFO, 0, 0);
   dynstr_set(glob_buffer, NULL);
@@ -1161,19 +1161,19 @@ int main(int argc,char *argv[])
     if (histfile)
     {
       if (verbose)
-        tee_fprintf(stdout, gettext("Reading history-file %s\n"),histfile);
+        tee_fprintf(stdout, _("Reading history-file %s\n"),histfile);
       read_history(histfile);
       if (!(histfile_tmp= (char*) my_malloc((uint) strlen(histfile) + 5,
                                             MYF(MY_WME))))
       {
-        fprintf(stderr, gettext("Couldn't allocate memory for temp histfile!\n"));
+        fprintf(stderr, _("Couldn't allocate memory for temp histfile!\n"));
         exit(1);
       }
       sprintf(histfile_tmp, "%s.TMP", histfile);
     }
   }
   sprintf(buff, "%s",
-          gettext("Type 'help;' or '\\h' for help. Type '\\c' to clear the buffer.\n"));
+          _("Type 'help;' or '\\h' for help. Type '\\c' to clear the buffer.\n"));
 
   put_info(buff,INFO_INFO,0,0);
   status.exit_status= read_and_execute(!status.batch);
@@ -1192,7 +1192,7 @@ sig_handler drizzle_end(int sig)
   {
     /* write-history */
     if (verbose)
-      tee_fprintf(stdout, gettext("Writing history-file %s\n"),histfile);
+      tee_fprintf(stdout, _("Writing history-file %s\n"),histfile);
     if (!write_history(histfile_tmp))
       my_rename(histfile_tmp, histfile, MYF(MY_WME));
   }
@@ -1201,7 +1201,7 @@ sig_handler drizzle_end(int sig)
   free_root(&hash_mem_root,MYF(0));
 
   if (sig >= 0)
-    put_info(sig ? gettext("Aborted") : gettext("Bye"), INFO_RESULT,0,0);
+    put_info(sig ? _("Aborted") : _("Bye"), INFO_RESULT,0,0);
   if (glob_buffer)
     dynstr_free(glob_buffer);
   my_free(glob_buffer, MYF(MY_ALLOW_ZERO_PTR));
@@ -1254,7 +1254,7 @@ sig_handler handle_sigint(int sig)
   sprintf(kill_buffer, "KILL /*!50000 QUERY */ %u", drizzle_thread_id(&drizzle));
   drizzle_real_query(kill_drizzle, kill_buffer, strlen(kill_buffer));
   drizzle_close(kill_drizzle);
-  tee_fprintf(stdout, gettext("Query aborted by Ctrl+C\n"));
+  tee_fprintf(stdout, _("Query aborted by Ctrl+C\n"));
 
   interrupted_query= 1;
 
@@ -1463,17 +1463,17 @@ static void usage(int version)
 {
   const char* readline= "readline";
 
-  printf(gettext("%s  Ver %s Distrib %s, for %s (%s) using %s %s\n"),
+  printf(_("%s  Ver %s Distrib %s, for %s (%s) using %s %s\n"),
          my_progname, VER, MYSQL_SERVER_VERSION, SYSTEM_TYPE, MACHINE_TYPE,
          readline, rl_library_version);
 
   if (version)
     return;
-  printf(gettext("\
+  printf(_("\
 Copyright (C) 2000-2008 MySQL AB\n                                      \
 This software comes with ABSOLUTELY NO WARRANTY. This is free software,\n \
 and you are welcome to modify and redistribute it under the GPL license\n"));
-  printf(gettext("Usage: %s [OPTIONS] [database]\n"), my_progname);
+  printf(_("Usage: %s [OPTIONS] [database]\n"), my_progname);
   my_print_help(my_long_options);
   print_defaults("my", load_default_groups);
   my_print_variables(my_long_options);
@@ -1506,7 +1506,7 @@ get_one_option(int optid, const struct my_option *opt __attribute__((unused)),
       }
       else
       {
-        put_info(gettext("DELIMITER cannot contain a backslash character"),
+        put_info(_("DELIMITER cannot contain a backslash character"),
                  INFO_ERROR,0,0);
         return 0;
       }
@@ -1527,7 +1527,7 @@ get_one_option(int optid, const struct my_option *opt __attribute__((unused)),
       init_tee(argument);
     break;
   case OPT_NOTEE:
-    printf(gettext("WARNING: option deprecated; use --disable-tee instead.\n"));
+    printf(_("WARNING: option deprecated; use --disable-tee instead.\n"));
     if (opt_outfile)
       end_tee();
     break;
@@ -1550,7 +1550,7 @@ get_one_option(int optid, const struct my_option *opt __attribute__((unused)),
     }
     break;
   case OPT_NOPAGER:
-    printf(gettext("WARNING: option deprecated; use --disable-pager instead.\n"));
+    printf(_("WARNING: option deprecated; use --disable-pager instead.\n"));
     opt_nopager= 1;
     break;
   case OPT_DRIZZLE_PROTOCOL:
@@ -1929,7 +1929,7 @@ static bool add_line(DYNAMIC_STRING *buffer,char *line,char *in_string,
       }
       else
       {
-        sprintf(buff,gettext("Unknown command '\\%c'."),inchar);
+        sprintf(buff,_("Unknown command '\\%c'."),inchar);
         if (put_info(buff,INFO_ERROR,0,0) > 0)
           return(1);
         *out++='\\';
@@ -2359,7 +2359,7 @@ static void build_completion_hash(bool rehash, bool write_info)
     {
       if (drizzle_num_rows(tables) > 0 && !opt_silent && write_info)
       {
-        tee_fprintf(stdout, gettext("\
+        tee_fprintf(stdout, _("\
 Reading table information for completion of table and column names\n    \
 You can turn off this feature to get a quicker startup with -A\n\n"));
       }
@@ -2456,13 +2456,13 @@ static int reconnect(void)
   /* purecov: begin tested */
   if (opt_reconnect)
   {
-    put_info(gettext("No connection. Trying to reconnect..."),INFO_INFO,0,0);
+    put_info(_("No connection. Trying to reconnect..."),INFO_INFO,0,0);
     (void) com_connect((DYNAMIC_STRING *) 0, 0);
     if (opt_rehash)
       com_rehash(NULL, NULL);
   }
   if (!connected)
-    return put_info(gettext("Can't connect to the server\n"),INFO_ERROR,0,0);
+    return put_info(_("Can't connect to the server\n"),INFO_ERROR,0,0);
   /* purecov: end */
   return 0;
 }
@@ -2519,7 +2519,7 @@ static void print_help_item(DRIZZLE_ROW *cur, int num_name, int num_cat, char *l
   char ccat= (*cur)[num_cat][0];
   if (*last_char != ccat)
   {
-    put_info(ccat == 'Y' ? gettext("categories:") : gettext("topics:"), INFO_INFO,0,0);
+    put_info(ccat == 'Y' ? _("categories:") : _("topics:"), INFO_INFO,0,0);
     *last_char= ccat;
   }
   tee_fprintf(PAGER, "   %s\n", (*cur)[num_name]);
@@ -2570,10 +2570,10 @@ static int com_server_help(DYNAMIC_STRING *buffer,
       }
 
       init_pager();
-      tee_fprintf(PAGER,   gettext("Name: \'%s\'\n"), cur[0]);
-      tee_fprintf(PAGER,   gettext("Description:\n%s"), cur[1]);
+      tee_fprintf(PAGER,   _("Name: \'%s\'\n"), cur[0]);
+      tee_fprintf(PAGER,   _("Description:\n%s"), cur[1]);
       if (cur[2] && *((char*)cur[2]))
-        tee_fprintf(PAGER, gettext("Examples:\n%s"), cur[2]);
+        tee_fprintf(PAGER, _("Examples:\n%s"), cur[2]);
       tee_fprintf(PAGER,   "\n");
       end_pager();
     }
@@ -2586,15 +2586,15 @@ static int com_server_help(DYNAMIC_STRING *buffer,
 
       if (num_fields == 2)
       {
-        put_info(gettext("Many help items for your request exist."), INFO_INFO,0,0);
-        put_info(gettext("To make a more specific request, please type 'help <item>',\nwhere <item> is one of the following"), INFO_INFO,0,0);
+        put_info(_("Many help items for your request exist."), INFO_INFO,0,0);
+        put_info(_("To make a more specific request, please type 'help <item>',\nwhere <item> is one of the following"), INFO_INFO,0,0);
         num_name= 0;
         num_cat= 1;
       }
       else if ((cur= drizzle_fetch_row(result)))
       {
-        tee_fprintf(PAGER, gettext("You asked for help about help category: '%s'\n"), cur[0]);
-        put_info(gettext("For more information, type 'help <item>', where <item> is one of the following"), INFO_INFO,0,0);
+        tee_fprintf(PAGER, _("You asked for help about help category: '%s'\n"), cur[0]);
+        put_info(_("For more information, type 'help <item>', where <item> is one of the following"), INFO_INFO,0,0);
         num_name= 1;
         num_cat= 2;
         print_help_item(&cur,1,2,&last_char);
@@ -2607,8 +2607,8 @@ static int com_server_help(DYNAMIC_STRING *buffer,
     }
     else
     {
-      put_info(gettext("\nNothing found"), INFO_INFO,0,0);
-      put_info(gettext("Please try to run 'help contents' for a list of all accessible topics\n"), INFO_INFO,0,0);
+      put_info(_("\nNothing found"), INFO_INFO,0,0);
+      put_info(_("Please try to run 'help contents' for a list of all accessible topics\n"), INFO_INFO,0,0);
     }
   }
 
@@ -2631,9 +2631,9 @@ com_help(DYNAMIC_STRING *buffer __attribute__((unused)),
       return com_server_help(buffer,line,help_arg);
   }
 
-  put_info(gettext("List of all Drizzle commands:"), INFO_INFO,0,0);
+  put_info(_("List of all Drizzle commands:"), INFO_INFO,0,0);
   if (!named_cmds)
-    put_info(gettext("Note that all text commands must be first on line and end with ';'"),INFO_INFO,0,0);
+    put_info(_("Note that all text commands must be first on line and end with ';'"),INFO_INFO,0,0);
   for (i = 0; commands[i].name; i++)
   {
     end= strmov(buff, commands[i].name);
@@ -2641,10 +2641,10 @@ com_help(DYNAMIC_STRING *buffer __attribute__((unused)),
       end= strmov(end, " ");
     if (commands[i].func)
       tee_fprintf(stdout, "%s(\\%c) %s\n", buff,
-                  commands[i].cmd_char, gettext(commands[i].doc));
+                  commands[i].cmd_char, _(commands[i].doc));
   }
   if (connected && drizzle_get_server_version(&drizzle) >= 40100)
-    put_info(gettext("\nFor server side help, type 'help contents'\n"), INFO_INFO,0,0);
+    put_info(_("\nFor server side help, type 'help contents'\n"), INFO_INFO,0,0);
   return 0;
 }
 
@@ -2667,7 +2667,7 @@ com_charset(DYNAMIC_STRING *buffer __attribute__((unused)), char *line)
   param= get_arg(buff, 0);
   if (!param || !*param)
   {
-    return put_info(gettext("Usage: \\C char_setname | charset charset_name"),
+    return put_info(_("Usage: \\C char_setname | charset charset_name"),
                     INFO_ERROR, 0, 0);
   }
   new_cs= get_charset_by_csname(param, MY_CS_PRIMARY, MYF(MY_WME));
@@ -2677,9 +2677,9 @@ com_charset(DYNAMIC_STRING *buffer __attribute__((unused)), char *line)
     drizzle_set_character_set(&drizzle, charset_info->csname);
     default_charset= (char *)charset_info->csname;
     default_charset_used= 1;
-    put_info(gettext("Charset changed"), INFO_INFO,0,0);
+    put_info(_("Charset changed"), INFO_INFO,0,0);
   }
-  else put_info(gettext("Charset is not found"), INFO_INFO,0,0);
+  else put_info(_("Charset is not found"), INFO_INFO,0,0);
   return 0;
 }
 
@@ -2710,7 +2710,7 @@ com_go(DYNAMIC_STRING *buffer,
     // Ignore empty quries
     if (status.batch)
       return 0;
-    return put_info(gettext("No query specified\n"),INFO_ERROR,0,0);
+    return put_info(_("No query specified\n"),INFO_ERROR,0,0);
 
   }
   if (!connected && reconnect())
@@ -2725,7 +2725,7 @@ com_go(DYNAMIC_STRING *buffer,
   if (skip_updates &&
       ((buffer->length < 4) || !strncmp(buffer->str, "SET ", 4)))
   {
-    (void) put_info(gettext("Ignoring query to other database"),INFO_INFO,0,0);
+    (void) put_info(_("Ignoring query to other database"),INFO_INFO,0,0);
     return 0;
   }
 
@@ -2774,7 +2774,7 @@ com_go(DYNAMIC_STRING *buffer,
     {
       if (!drizzle_num_rows(result) && ! quick && !column_types_flag)
       {
-        strmov(buff, gettext("Empty set"));
+        strmov(buff, _("Empty set"));
         if (opt_xml)
         {
           /*
@@ -2810,7 +2810,7 @@ com_go(DYNAMIC_STRING *buffer,
       }
     }
     else if (drizzle_affected_rows(&drizzle) == ~(uint64_t) 0)
-      strmov(buff,gettext("Query OK"));
+      strmov(buff,_("Query OK"));
     else
       sprintf(buff, ngettext("Query OK, %ld row affected",
                              "Query OK, %ld rows affected",
