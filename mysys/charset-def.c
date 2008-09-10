5732153e3a0e15898ee4659ef0cd36bf0724d653
Brian Aker <brian@tangent.org>
2008-09-09 21:19:48 -0700

    Cleanup of character sets.

--- mysys/charset-def.c
+++ mysys/charset-def.c
@@ -78,7 +78,6 @@ bool init_compiled_charsets(myf flags __attribute__((unused)))
   
   add_compiled_collation(&my_charset_latin1);
   add_compiled_collation(&my_charset_latin1_bin);
-  add_compiled_collation(&my_charset_latin1_german2_ci);
 
 #ifdef HAVE_CHARSET_utf8mb3
   add_compiled_collation(&my_charset_utf8mb3_general_ci);
