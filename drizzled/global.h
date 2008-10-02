87f48454a0f3167e37370a9e64936db0dea8c8ec
Monty <mordred@scylla.inaugust.com>
2008-10-02 07:34:18 -0700

    Made including stdint.h work.

--- drizzled/global.h
+++ drizzled/global.h
@@ -80,6 +80,7 @@
 #endif
 
 
+#define __EXTENSIONS__ 1
 /*
   Temporary solution to solve bug#7156. Include "sys/types.h" before
   the thread headers, else the function madvise() will not be defined
@@ -88,152 +89,14 @@
 #include <sys/types.h>
 #endif
 
-#define __EXTENSIONS__ 1  /* We want some extension */
-
 #if defined(HAVE_STDINT_H)
-/* Need to include this _before_ stdlib, so that all defines are right */
-/* We are mixing C and C++, so we wan the C limit macros in the C++ too */
-/* Enable some extra C99 extensions */
-#undef _STDINT_H
-#define __STDC_FORMAT_MACROS
+#include <stdint.h>
+#else
+#error "You must have stdint!"
+#endif
+
+#if defined(HAVE_INTTYPES_H)
 #include <inttypes.h>
-/*
-  We include the following because currently Google #$@#$ Protocol Buffers possibly break stdint defines.
-  Or I am wrong, very possible, and hope someone finds the solution.
-
-  Taken from /usr/include/stdint.h
-*/
-
-/* 7.18.2 Limits of specified-width integer types:
- *   These #defines specify the minimum and maximum limits
- *   of each of the types declared above.
- */
-
-
-#if (!defined(INT16_MAX))
-/* 7.18.2.1 Limits of exact-width integer types */
-#define INT8_MAX         127
-#define INT16_MAX        32767
-#define INT32_MAX        2147483647
-#define INT64_MAX        9223372036854775807
-
-#define INT8_MIN          -128
-#define INT16_MIN         -32768
-
-/*
-  Note:  the literal "most negative int" cannot be written in C --
-  the rules in the standard (section 6.4.4.1 in C99) will give it
-  an unsigned type, so INT32_MIN (and the most negative member of
-  any larger signed type) must be written via a constant expression.
-*/
-#define INT32_MIN        (-INT32_MAX-1)
-#define INT64_MIN        (-INT64_MAX-1)
-
-#define UINT8_MAX         255
-#define UINT16_MAX        65535
-#define UINT32_MAX        4294967295U
-#define UINT64_MAX        18446744073709551615U
-
-/* 7.18.2.2 Limits of minimum-width integer types */
-#define INT_LEAST8_MIN    INT8_MIN
-#define INT_LEAST16_MIN   INT16_MIN
-#define INT_LEAST32_MIN   INT32_MIN
-#define INT_LEAST64_MIN   INT64_MIN
-
-#define INT_LEAST8_MAX    INT8_MAX
-#define INT_LEAST16_MAX   INT16_MAX
-#define INT_LEAST32_MAX   INT32_MAX
-#define INT_LEAST64_MAX   INT64_MAX
-
-#define UINT_LEAST8_MAX   UINT8_MAX
-#define UINT_LEAST16_MAX  UINT16_MAX
-#define UINT_LEAST32_MAX  UINT32_MAX
-#define UINT_LEAST64_MAX  UINT64_MAX
-
-/* 7.18.2.3 Limits of fastest minimum-width integer types */
-#define INT_FAST8_MIN     INT8_MIN
-#define INT_FAST16_MIN    INT16_MIN
-#define INT_FAST32_MIN    INT32_MIN
-#define INT_FAST64_MIN    INT64_MIN
-
-#define INT_FAST8_MAX     INT8_MAX
-#define INT_FAST16_MAX    INT16_MAX
-#define INT_FAST32_MAX    INT32_MAX
-#define INT_FAST64_MAX    INT64_MAX
-
-#define UINT_FAST8_MAX    UINT8_MAX
-#define UINT_FAST16_MAX   UINT16_MAX
-#define UINT_FAST32_MAX   UINT32_MAX
-#define UINT_FAST64_MAX   UINT64_MAX
-
-/* 7.18.2.4 Limits of integer types capable of holding object pointers */
-
-#if __WORDSIZE == 64
-#define INTPTR_MIN    INT64_MIN
-#define INTPTR_MAX    INT64_MAX
-#else
-#define INTPTR_MIN        INT32_MIN
-#define INTPTR_MAX        INT32_MAX
-#endif
-
-#if __WORDSIZE == 64
-#define UINTPTR_MAX    UINT64_MAX
-#else
-#define UINTPTR_MAX       UINT32_MAX
-#endif
-
-/* 7.18.2.5 Limits of greatest-width integer types */
-#define INTMAX_MIN        INT64_MIN
-#define INTMAX_MAX        INT64_MAX
-
-#define UINTMAX_MAX       UINT64_MAX
-
-/* 7.18.3 "Other" */
-#if __WORDSIZE == 64
-#define PTRDIFF_MIN    INT64_MIN
-#define PTRDIFF_MAX    INT64_MAX
-#else
-#define PTRDIFF_MIN       INT32_MIN
-#define PTRDIFF_MAX       INT32_MAX
-#endif
-
-/* We have no sig_atomic_t yet, so no SIG_ATOMIC_{MIN,MAX}.
-   Should end up being {-127,127} or {0,255} ... or bigger.
-   My bet would be on one of {U}INT32_{MIN,MAX}. */
-
-#if __WORDSIZE == 64
-#define SIZE_MAX    UINT64_MAX
-#else
-#define SIZE_MAX          UINT32_MAX
-#endif
-
-#ifndef WCHAR_MAX
-#  ifdef __WCHAR_MAX__
-#    define WCHAR_MAX     __WCHAR_MAX__
-#  else
-#    define WCHAR_MAX     0x7fffffff
-#  endif
-#endif
-
-/* WCHAR_MIN should be 0 if wchar_t is an unsigned type and
-   (-WCHAR_MAX-1) if wchar_t is a signed type.  Unfortunately,
-   it turns out that -fshort-wchar changes the signedness of
-   the type. */
-#ifndef WCHAR_MIN
-#  if WCHAR_MAX == 0xffff
-#    define WCHAR_MIN       0
-#  else
-#    define WCHAR_MIN       (-WCHAR_MAX-1)
-#  endif
-#endif
-
-#define WINT_MIN    INT32_MIN
-#define WINT_MAX    INT32_MAX
-
-#define SIG_ATOMIC_MIN    INT32_MIN
-#define SIG_ATOMIC_MAX    INT32_MAX
-#endif
-
 #else
 #error "You must have inttypes!"
 #endif
