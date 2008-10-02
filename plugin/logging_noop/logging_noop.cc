b5315582d5032c5c98d71d5966404b19ff1ed63d
Mark Atwood <mark@fallenpegasus.com>
2008-10-02 13:56:30 -0700

    more make plug logging work

--- plugin/logging_noop/logging_noop.cc
+++ plugin/logging_noop/logging_noop.cc
@@ -7,7 +7,34 @@
 
 #define MAX_MSG_LEN (32*1024)
 
-static int fd = -1;
+static int fd= -1;
+
+// copied from drizzled/sql_parse.cc
+
+const LEX_STRING command_name[]={
+  { C_STRING_WITH_LEN("Sleep") },
+  { C_STRING_WITH_LEN("Quit") },
+  { C_STRING_WITH_LEN("InitDB") },
+  { C_STRING_WITH_LEN("Query") },
+  { C_STRING_WITH_LEN("FieldList") },
+  { C_STRING_WITH_LEN("CreateDB") },
+  { C_STRING_WITH_LEN("DropDB") },
+  { C_STRING_WITH_LEN("Refresh") },
+  { C_STRING_WITH_LEN("Shutdown") },
+  { C_STRING_WITH_LEN("Processlist") },
+  { C_STRING_WITH_LEN("Connect") },
+  { C_STRING_WITH_LEN("Kill") },
+  { C_STRING_WITH_LEN("Ping") },
+  { C_STRING_WITH_LEN("Time") },
+  { C_STRING_WITH_LEN("ChangeUser") },
+  { C_STRING_WITH_LEN("BinlogDump") },
+  { C_STRING_WITH_LEN("ConnectOut") },
+  { C_STRING_WITH_LEN("RegisterSlave") },
+  { C_STRING_WITH_LEN("SetOption") },
+  { C_STRING_WITH_LEN("Daemon") },
+  { C_STRING_WITH_LEN("Error") }
+};
+
 
 bool logging_noop_func_pre (THD *thd)
 {
@@ -20,12 +47,11 @@ bool logging_noop_func_pre (THD *thd)
 
   msgbuf_len=
     snprintf(msgbuf, MAX_MSG_LEN,
-	     "log bgn thread_id=%ld stmt_id=%ld query_id=%ld command=%d"
+	     "log bgn thread_id=%ld query_id=%ld command=%.*s"
 	     " db=\"%.*s\" query=\"%.*s\"\n",
 	     (unsigned long) thd->thread_id,
-	     thd->id,
 	     (unsigned long) thd->query_id,
-	     thd->command,
+	     command_name[thd->command].length, command_name[thd->command].str,
 	     thd->db_length, thd->db,
 	     thd->query_length, thd->query);
   wrv= write(fd, msgbuf, msgbuf_len);
@@ -45,12 +71,11 @@ bool logging_noop_func_post (THD *thd)
 
   msgbuf_len=
     snprintf(msgbuf, MAX_MSG_LEN,
-	     "log end thread_id=%ld stmt_id=%ld query_id=%ld command=%d"
+	     "log end thread_id=%ld query_id=%ld command=%.*s"
 	     " utime=%lld rows.sent=%ld rows.exam=%ld\n",
 	     (unsigned long) thd->thread_id, 
-	     thd->id,
 	     (unsigned long) thd->query_id,
-	     thd->command,
+	     command_name[thd->command].length, command_name[thd->command].str,
 	     (thd->current_utime() - thd->start_utime),
 	     (unsigned long) thd->sent_row_count,
 	     (unsigned long) thd->examined_row_count);
@@ -70,8 +95,13 @@ static int logging_noop_plugin_init(void *p)
   l->logging_pre= logging_noop_func_pre;
   l->logging_post= logging_noop_func_post;
 
-  fd = open("/tmp/drizzle.log", O_WRONLY | O_APPEND);
-  if (fd < 0) return fd;
+  fd= open("/tmp/drizzle.log", O_WRONLY | O_APPEND);
+  if (fd < 0) {
+    fprintf(stderr,
+	    "MRA fail open /tmp/drizzle.log fd=%d er=%s\n",
+	    fd, strerror(errno));
+    return fd;
+  }
 
   /* need to do something better with the fd */
 
