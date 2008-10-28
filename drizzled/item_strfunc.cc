d53bc3ca4e28ddc91e8baf2c0966ee855759290a
Brian Aker <brian@tangent.org>
2008-10-28 06:45:31 -0700

    New UUID code. Also changed default scaling for "8" core machine instead of 20 core machine.
    Who the hell builds a 20 core machine? Stupid default.

--- drizzled/item_strfunc.cc
+++ drizzled/item_strfunc.cc
@@ -30,6 +30,7 @@ using namespace std;
 #include <mysys/sha1.h>
 #include <zlib.h>
 #include <drizzled/query_id.h>
+#include <uuid/uuid.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -2499,116 +2500,20 @@ null:
   return 0;
 }
 
-/*
-  UUID, as in
-    DCE 1.1: Remote Procedure Call,
-    Open Group Technical Standard Document Number C706, October 1997,
-    (supersedes C309 DCE: Remote Procedure Call 8/1994,
-    which was basis for ISO/IEC 11578:1996 specification)
-*/
 
-static struct rand_struct uuid_rand;
-static uint32_t nanoseq;
-static uint64_t uuid_time=0;
-static char clock_seq_and_node_str[]="-0000-000000000000";
-
-/**
-  number of 100-nanosecond intervals between
-  1582-10-15 00:00:00.00 and 1970-01-01 00:00:00.00.
-*/
-#define UUID_TIME_OFFSET ((uint64_t) 141427 * 24 * 60 * 60 * 1000 * 10 )
-
-#define UUID_VERSION      0x1000
-#define UUID_VARIANT      0x8000
-
-static void tohex(char *to, uint32_t from, uint32_t len)
-{
-  to+= len;
-  while (len--)
-  {
-    *--to= _dig_vec_lower[from & 15];
-    from >>= 4;
-  }
-}
-
-static void set_clock_seq_str()
-{
-  uint16_t clock_seq= ((uint)(my_rnd(&uuid_rand)*16383)) | UUID_VARIANT;
-  tohex(clock_seq_and_node_str+1, clock_seq, 4);
-  nanoseq= 0;
-}
 
 String *Item_func_uuid::val_str(String *str)
 {
-  assert(fixed == 1);
-  char *s;
-  Session *session= current_session;
+  uuid_t uu;
+  char *uuid_string;
 
-  pthread_mutex_lock(&LOCK_uuid_generator);
-  if (! uuid_time) /* first UUID() call. initializing data */
-  {
-    ulong tmp= sql_rnd();
-    unsigned char mac[6];
-    int i;
-    const Query_id& query_id= Query_id::get_query_id();
-    if (my_gethwaddr(mac))
-    {
-      /* purecov: begin inspected */
-      /*
-        generating random "hardware addr"
-        and because specs explicitly specify that it should NOT correlate
-        with a clock_seq value (initialized random below), we use a separate
-        randominit() here
-      */
-      randominit(&uuid_rand, tmp + (ulong) session, tmp + query_id.value());
-      for (i=0; i < (int)sizeof(mac); i++)
-        mac[i]=(unsigned char)(my_rnd(&uuid_rand)*255);
-      /* purecov: end */    
-    }
-    s=clock_seq_and_node_str+sizeof(clock_seq_and_node_str)-1;
-    for (i=sizeof(mac)-1 ; i>=0 ; i--)
-    {
-      *--s=_dig_vec_lower[mac[i] & 15];
-      *--s=_dig_vec_lower[mac[i] >> 4];
-    }
-    randominit(&uuid_rand, tmp + (ulong) server_start_time,
-	       tmp + (ulong) session->status_var.bytes_sent);
-    set_clock_seq_str();
-  }
-
-  uint64_t tv=my_getsystime() + UUID_TIME_OFFSET + nanoseq;
-  if (unlikely(tv < uuid_time))
-    set_clock_seq_str();
-  else if (unlikely(tv == uuid_time))
-  {
-    /* special protection from low-res system clocks */
-    nanoseq++;
-    tv++;
-  }
-  else
-  {
-    if (nanoseq)
-    {
-      tv-=nanoseq;
-      nanoseq=0;
-    }
-    assert(tv > uuid_time);
-  }
-  uuid_time=tv;
-  pthread_mutex_unlock(&LOCK_uuid_generator);
-
-  uint32_t time_low=            (uint32_t) (tv & 0xFFFFFFFF);
-  uint16_t time_mid=            (uint16_t) ((tv >> 32) & 0xFFFF);
-  uint16_t time_hi_and_version= (uint16_t) ((tv >> 48) | UUID_VERSION);
-
-  str->realloc(UUID_LENGTH+1);
-  str->length(UUID_LENGTH);
+  /* 36 characters for uuid string +1 for NULL */
+  str->realloc(36+1);
+  str->length(36);
   str->set_charset(system_charset_info);
-  s=(char *) str->ptr();
-  s[8]=s[13]='-';
-  tohex(s, time_low, 8);
-  tohex(s+9, time_mid, 4);
-  tohex(s+14, time_hi_and_version, 4);
-  my_stpcpy(s+18, clock_seq_and_node_str);
+  uuid_string= (char *) str->ptr();
+  uuid_generate_time(uu);
+  uuid_unparse(uu, uuid_string);
+
   return str;
 }
