913558ea1c25e169a5f7464158630b7ab8946c04
Monty Taylor <monty@inaugust.com>
2008-12-07 19:55:23 -0800

    Fixed db.cc strncpy removal.

--- drizzled/db.cc
+++ drizzled/db.cc
@@ -1320,10 +1320,11 @@ bool mysql_change_db(Session *session, const LEX_STRING *new_db_name, bool force
     TODO: fix check_db_name().
   */
 
-  new_db_file_name.str= (char *)malloc(new_db_name->length);
+  new_db_file_name.str= (char *)malloc(new_db_name->length+1);
   if (new_db_file_name.str == NULL)
     return true;
   strncpy(new_db_file_name.str, new_db_name->str, new_db_name->length);
+  new_db_file_name.str[new_db_name->length]= 0;
   new_db_file_name.length= new_db_name->length;
 
   /*
