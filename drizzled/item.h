4a715c37631b7ab9c55af597497a5c51b8cd5c85
Lee <lbieber@lbieber-desktop>
2008-12-09 11:33:58 -0800

    move functions from item.cc/item.h to item directory

--- drizzled/item.h
+++ drizzled/item.h
@@ -589,16 +589,22 @@ public:
 };
 
 #include <drizzled/item/basic_constant.h>
+#include <drizzled/item/bin_string.h>
+#include <drizzled/item/blob.h>
 #include <drizzled/item/ident.h>
 #include <drizzled/item/decimal.h>
+#include <drizzled/item/empty_string.h>
 #include <drizzled/item/field.h>
+#include <drizzled/item/hex_string.h>
 #include <drizzled/item/ifloat.h>
 #include <drizzled/item/istring.h>
 #include <drizzled/item/int.h>
 #include <drizzled/item/null.h>
 #include <drizzled/item/num.h>
 #include <drizzled/item/param.h>
+#include <drizzled/item/ref.h>
 #include <drizzled/item/return_date_time.h>
+#include <drizzled/item/return_int.h>
 #include <drizzled/item/uint.h>
 
 void mark_as_dependent(Session *session,
@@ -610,233 +616,6 @@ void mark_as_dependent(Session *session,
 Item** resolve_ref_in_select_and_group(Session *session,
 			               Item_ident *ref,
 				       st_select_lex *select);
-
-
-class Item_blob :public Item_string
-{
-public:
-  Item_blob(const char *name, uint32_t length) :
-    Item_string(name, length, &my_charset_bin)
-  { max_length= length; }
-  enum Type type() const { return TYPE_HOLDER; }
-  enum_field_types field_type() const { return DRIZZLE_TYPE_BLOB; }
-};
-
-
-/**
-  Item_empty_string -- is a utility class to put an item into List<Item>
-  which is then used in protocol.send_fields() when sending SHOW output to
-  the client.
-*/
-
-class Item_empty_string :public Item_string
-{
-public:
-  Item_empty_string(const char *header,uint32_t length, const CHARSET_INFO * cs= NULL) :
-    Item_string("",0, cs ? cs : &my_charset_utf8_general_ci)
-    { name=(char*) header; max_length= cs ? length * cs->mbmaxlen : length; }
-  void make_field(Send_field *field);
-};
-
-
-class Item_return_int :public Item_int
-{
-  enum_field_types int_field_type;
-public:
-  Item_return_int(const char *name_arg, uint32_t length,
-		  enum_field_types field_type_arg, int64_t value= 0)
-    :Item_int(name_arg, value, length), int_field_type(field_type_arg)
-  {
-    unsigned_flag=1;
-  }
-  enum_field_types field_type() const { return int_field_type; }
-};
-
-
-class Item_hex_string: public Item_basic_constant
-{
-public:
-  Item_hex_string() {}
-  Item_hex_string(const char *str,uint32_t str_length);
-  enum Type type() const { return VARBIN_ITEM; }
-  double val_real()
-  {
-    assert(fixed == 1);
-    return (double) (uint64_t) Item_hex_string::val_int();
-  }
-  int64_t val_int();
-  bool basic_const_item() const { return 1; }
-  String *val_str(String*) { assert(fixed == 1); return &str_value; }
-  my_decimal *val_decimal(my_decimal *);
-  int save_in_field(Field *field, bool no_conversions);
-  enum Item_result result_type () const { return STRING_RESULT; }
-  enum Item_result cast_to_int_type() const { return INT_RESULT; }
-  enum_field_types field_type() const { return DRIZZLE_TYPE_VARCHAR; }
-  virtual void print(String *str, enum_query_type query_type);
-  bool eq(const Item *item, bool binary_cmp) const;
-  virtual Item *safe_charset_converter(const CHARSET_INFO * const tocs);
-  bool check_vcol_func_processor(unsigned char *)
-  { return false; }
-};
-
-
-class Item_bin_string: public Item_hex_string
-{
-public:
-  Item_bin_string(const char *str,uint32_t str_length);
-};
-
-class Item_result_field :public Item	/* Item with result field */
-{
-public:
-  Field *result_field;				/* Save result here */
-  Item_result_field() :result_field(0) {}
-  // Constructor used for Item_sum/Item_cond_and/or (see Item comment)
-  Item_result_field(Session *session, Item_result_field *item):
-    Item(session, item), result_field(item->result_field)
-  {}
-  ~Item_result_field() {}			/* Required with gcc 2.95 */
-  Field *get_tmp_table_field() { return result_field; }
-  Field *tmp_table_field(Table *)
-  { return result_field; }
-  table_map used_tables() const { return 1; }
-  virtual void fix_length_and_dec()=0;
-  void set_result_field(Field *field) { result_field= field; }
-  bool is_result_field() { return 1; }
-  void save_in_result_field(bool no_conversions)
-  {
-    save_in_field(result_field, no_conversions);
-  }
-  void cleanup();
-  bool check_vcol_func_processor(unsigned char *)
-  { return false; }
-};
-
-
-class Item_ref :public Item_ident
-{
-protected:
-  void set_properties();
-public:
-  enum Ref_Type { REF, DIRECT_REF, OUTER_REF };
-  Field *result_field;			 /* Save result here */
-  Item **ref;
-  Item_ref(Name_resolution_context *context_arg,
-           const char *db_arg, const char *table_name_arg,
-           const char *field_name_arg)
-    :Item_ident(context_arg, db_arg, table_name_arg, field_name_arg),
-     result_field(0), ref(0) {}
-  /*
-    This constructor is used in two scenarios:
-    A) *item = NULL
-      No initialization is performed, fix_fields() call will be necessary.
-
-    B) *item points to an Item this Item_ref will refer to. This is
-      used for GROUP BY. fix_fields() will not be called in this case,
-      so we call set_properties to make this item "fixed". set_properties
-      performs a subset of action Item_ref::fix_fields does, and this subset
-      is enough for Item_ref's used in GROUP BY.
-
-    TODO we probably fix a superset of problems like in BUG#6658. Check this
-         with Bar, and if we have a more broader set of problems like this.
-  */
-  Item_ref(Name_resolution_context *context_arg, Item **item,
-           const char *table_name_arg, const char *field_name_arg,
-           bool alias_name_used_arg= false);
-
-  /* Constructor need to process subselect with temporary tables (see Item) */
-  Item_ref(Session *session, Item_ref *item)
-    :Item_ident(session, item), result_field(item->result_field), ref(item->ref) {}
-  enum Type type() const		{ return REF_ITEM; }
-  bool eq(const Item *item, bool binary_cmp) const
-  {
-    Item *it= ((Item *) item)->real_item();
-    return ref && (*ref)->eq(it, binary_cmp);
-  }
-  double val_real();
-  int64_t val_int();
-  my_decimal *val_decimal(my_decimal *);
-  bool val_bool();
-  String *val_str(String* tmp);
-  bool is_null();
-  bool get_date(DRIZZLE_TIME *ltime,uint32_t fuzzydate);
-  double val_result();
-  int64_t val_int_result();
-  String *str_result(String* tmp);
-  my_decimal *val_decimal_result(my_decimal *);
-  bool val_bool_result();
-  bool send(Protocol *prot, String *tmp);
-  void make_field(Send_field *field);
-  bool fix_fields(Session *, Item **);
-  void fix_after_pullout(st_select_lex *new_parent, Item **ref);
-  int save_in_field(Field *field, bool no_conversions);
-  void save_org_in_field(Field *field);
-  enum Item_result result_type () const { return (*ref)->result_type(); }
-  enum_field_types field_type() const   { return (*ref)->field_type(); }
-  Field *get_tmp_table_field()
-  { return result_field ? result_field : (*ref)->get_tmp_table_field(); }
-  Item *get_tmp_table_item(Session *session);
-  table_map used_tables() const
-  {
-    return depended_from ? OUTER_REF_TABLE_BIT : (*ref)->used_tables();
-  }
-  void update_used_tables()
-  {
-    if (!depended_from)
-      (*ref)->update_used_tables();
-  }
-  table_map not_null_tables() const { return (*ref)->not_null_tables(); }
-  void set_result_field(Field *field)	{ result_field= field; }
-  bool is_result_field() { return 1; }
-  void save_in_result_field(bool no_conversions)
-  {
-    (*ref)->save_in_field(result_field, no_conversions);
-  }
-  Item *real_item()
-  {
-    return ref ? (*ref)->real_item() : this;
-  }
-  bool walk(Item_processor processor, bool walk_subquery, unsigned char *arg)
-  { return (*ref)->walk(processor, walk_subquery, arg); }
-  virtual void print(String *str, enum_query_type query_type);
-  bool result_as_int64_t()
-  {
-    return (*ref)->result_as_int64_t();
-  }
-  void cleanup();
-  Item_field *filed_for_view_update()
-    { return (*ref)->filed_for_view_update(); }
-  virtual Ref_Type ref_type() { return REF; }
-
-  // Row emulation: forwarding of ROW-related calls to ref
-  uint32_t cols()
-  {
-    return ref && result_type() == ROW_RESULT ? (*ref)->cols() : 1;
-  }
-  Item* element_index(uint32_t i)
-  {
-    return ref && result_type() == ROW_RESULT ? (*ref)->element_index(i) : this;
-  }
-  Item** addr(uint32_t i)
-  {
-    return ref && result_type() == ROW_RESULT ? (*ref)->addr(i) : 0;
-  }
-  bool check_cols(uint32_t c)
-  {
-    return ref && result_type() == ROW_RESULT ? (*ref)->check_cols(c)
-                                              : Item::check_cols(c);
-  }
-  bool null_inside()
-  {
-    return ref && result_type() == ROW_RESULT ? (*ref)->null_inside() : 0;
-  }
-  void bring_value()
-  {
-    if (ref && result_type() == ROW_RESULT)
-      (*ref)->bring_value();
-  }
-
-};
 
 
 /*
