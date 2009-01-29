a5f920e025d89844383543c4ea818624ac4e55bf
Jay Pipes <jpipes@serialcoder>
2009-01-29 17:00:06 -0500

    Fixes MICROSECOND() to use new Temporal system and throw appropriate errors on bad datetimes.  Adds new microsecond.test case.  Still to do: don't have microseconds thrown away by DATETIME type... :)

--- drizzled/function/time/microsecond.cc
+++ drizzled/function/time/microsecond.cc
@@ -17,24 +17,93 @@
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#include <drizzled/server_includes.h>
+#include "drizzled/server_includes.h"
 #include CSTDINT_H
-#include <drizzled/function/time/microsecond.h>
-
-/**
-  MICROSECOND(a) is a function ( extraction) that extracts the microseconds
-  from a.
-
-  a: Datetime or time value
-  Result: int value
-*/
+#include "drizzled/temporal.h"
+#include "drizzled/error.h"
+#include "drizzled/function/time/microsecond.h"
 
 int64_t Item_func_microsecond::val_int()
 {
-  assert(fixed == 1);
-  DRIZZLE_TIME ltime;
-  if (!get_arg0_time(&ltime))
-    return ltime.second_part;
-  return 0;
-}
+  assert(fixed);
 
+  if (args[0]->is_null())
+  {
+    /* For NULL argument, we return a NULL result */
+    null_value= true;
+    return 0;
+  }
+
+  /* 
+   * Because of the ridiculous way in which MySQL handles
+   * TIME values (it does implicit integer -> string conversions
+   * but only for DATETIME, not TIME values) we must first 
+   * try a conversion into a TIME from a string.  If this
+   * fails, we fall back on a DATETIME conversion.  This is
+   * necessary because of the fact that DateTime::from_string()
+   * looks first for DATETIME, then DATE regex matches.  6 consecutive
+   * numbers, say 231130, will match the DATE regex YYMMDD
+   * with no TIME part, but MySQL actually implicitly treats
+   * parameters to SECOND(), HOUR(), and MINUTE() as TIME-only
+   * values and matches 231130 as HHmmSS!
+   *
+   * Oh, and Brian Aker MADE me do this. :) --JRP
+   */
+  drizzled::Time temporal_time;
+  
+  char time_buff[DRIZZLE_MAX_LENGTH_DATETIME_AS_STRING];
+  String tmp_time(time_buff,sizeof(time_buff), &my_charset_utf8_bin);
+  String *time_res= args[0]->val_str(&tmp_time);
+  if (! temporal_time.from_string(time_res->c_ptr(), time_res->length()))
+  {
+    /* 
+     * OK, we failed to match the first argument as a string
+     * representing a time value, so we grab the first argument 
+     * as a DateTime object and try that for a match...
+     */
+    drizzled::DateTime temporal_datetime;
+    Item_result arg0_result_type= args[0]->result_type();
+    
+    switch (arg0_result_type)
+    {
+      case STRING_RESULT:
+        {
+          char buff[DRIZZLE_MAX_LENGTH_DATETIME_AS_STRING];
+          String tmp(buff,sizeof(buff), &my_charset_utf8_bin);
+          String *res= args[0]->val_str(&tmp);
+          if (! temporal_datetime.from_string(res->c_ptr(), res->length()))
+          {
+            /* 
+            * Could not interpret the function argument as a temporal value, 
+            * so throw an error and return 0
+            */
+            my_error(ER_INVALID_DATETIME_VALUE, MYF(0), res->c_ptr());
+            return 0;
+          }
+        }
+        break;
+      case INT_RESULT:
+        if (temporal_datetime.from_int64_t(args[0]->val_int()))
+          break;
+        /* Intentionally fall-through on invalid conversion from integer */
+      default:
+        {
+          /* 
+          * Could not interpret the function argument as a temporal value, 
+          * so throw an error and return 0
+          */
+          null_value= true;
+          char buff[DRIZZLE_MAX_LENGTH_DATETIME_AS_STRING];
+          String tmp(buff,sizeof(buff), &my_charset_utf8_bin);
+          String *res;
+
+          res= args[0]->val_str(&tmp);
+
+          my_error(ER_INVALID_DATETIME_VALUE, MYF(0), res->c_ptr());
+          return 0;
+        }
+    }
+    return (int64_t) temporal_datetime.useconds();
+  }
+  return (int64_t) temporal_time.useconds();
+}
