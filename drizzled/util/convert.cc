140607ee22b508e6a4cbb158d3375437782de1a6
Eric Day <eday@oddments.org>
2009-04-05 10:35:26 -0700

    Added new convert file for hex function.

---
+++ drizzled/util/convert.cc
@@ -0,0 +1,37 @@
+/* - mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; -*-
+ *  vim:expandtab:shiftwidth=2:tabstop=2:smarttab:
+ *
+ *  Copyright (C) 2009 Sun Microsystems
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <drizzled/util/convert.h>
+
+uint64_t drizzled_string_to_hex(char *to, const char *from, uint64_t from_size)
+{
+  static const char hex_map[]= "0123456789ABCDEF";
+  const char *from_end;
+
+  for (from_end= from + from_size; from != from_end; from++)
+  {
+    *to++= hex_map[((unsigned char) *from) >> 4];
+    *to++= hex_map[((unsigned char) *from) & 0xF];
+  }
+
+  *to= 0;
+
+  return from_size * 2;
+}
