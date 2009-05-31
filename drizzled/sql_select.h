f04a468a18a94f0dcb73d7e4ff11f8358a4221ab
Jay Pipes <jpipes@serialcoder>
2009-05-31 13:49:30 -0400

    Phase 2 of JOIN refactoring.
    * Moved functions out of sql_select.cc into join.cc where the function primarily
      involved only the JOIN class into drizzled/join.cc.  This effort is necessary 
      in the move towards a more object-oriented coding style for the nested loops 
      join algorithm and Jasekidd's GSOC project.
    * Pulled many statically-defined functions out of sql_select.cc and into the 
      global namespace where it made sense to do so (used in >1 file).
    * Code style cleanup

--- drizzled/sql_select.h
+++ drizzled/sql_select.h
@@ -204,7 +204,6 @@ typedef enum_nested_loop_state
 typedef int (*Read_record_func)(struct st_join_table *tab);
 Next_select_func setup_end_select_func(JOIN *join);
 
-
 typedef struct st_join_table {
   st_join_table() {}                          /* Remove gcc warning */
   Table		*table;
@@ -395,6 +394,63 @@ typedef struct st_select_check {
   uint32_t const_ref,reg_ref;
 } SELECT_CHECK;
 
+/*
+   This structure is used to collect info on potentially sargable
+   predicates in order to check whether they become sargable after
+   reading const tables.
+   We form a bitmap of indexes that can be used for sargable predicates.
+   Only such indexes are involved in range analysis.
+*/
+typedef struct st_sargable_param
+{
+  Field *field;              /* field against which to check sargability */
+  Item **arg_value;          /* values of potential keys for lookups     */
+  uint32_t num_values;           /* number of values in the above array      */
+} SARGABLE_PARAM;
+
+/// Used when finding key fields
+typedef struct key_field_t {
+  Field		*field;
+  Item		*val;			///< May be empty if diff constant
+  uint		level;
+  uint		optimize; // KEY_OPTIMIZE_*
+  bool		eq_func;
+  /**
+    If true, the condition this struct represents will not be satisfied
+    when val IS NULL.
+  */
+  bool          null_rejecting;
+  bool          *cond_guard; /* See KEYUSE::cond_guard */
+  uint32_t          sj_pred_no; /* See KEYUSE::sj_pred_no */
+} KEY_FIELD;
+
+/*****************************************************************************
+  Make som simple condition optimization:
+  If there is a test 'field = const' change all refs to 'field' to 'const'
+  Remove all dummy tests 'item = item', 'const op const'.
+  Remove all 'item is NULL', when item can never be null!
+  item->marker should be 0 for all items on entry
+  Return in cond_value false if condition is impossible (1 = 2)
+*****************************************************************************/
+class COND_CMP :public ilink {
+public:
+  static void *operator new(size_t size)
+  {
+    return (void*) sql_alloc((uint32_t) size);
+  }
+  static void operator delete(void *, size_t)
+  { TRASH(ptr, size); }
+
+  Item *and_level;
+  Item_func *cmp_func;
+  COND_CMP(Item *a,Item_func *b) :and_level(a),cmp_func(b) {}
+};
+
+#ifdef HAVE_EXPLICIT_TEMPLATE_INSTANTIATION
+template class I_List<COND_CMP>;
+template class I_List_iterator<COND_CMP>;
+#endif
+
 extern const char *join_type_str[];
 void TEST_join(JOIN *join);
 
@@ -416,6 +472,124 @@ void copy_funcs(Item **func_ptr);
 Field* create_tmp_field_from_field(Session *session, Field* org_field,
                                    const char *name, Table *table,
                                    Item_field *item, uint32_t convert_blob_length);
+bool test_if_ref(Item_field *left_item,Item *right_item);
+COND *optimize_cond(JOIN *join, COND *conds, List<TableList> *join_list, Item::cond_result *cond_value);
+COND *make_cond_for_table(COND *cond,table_map table, table_map used_table, bool exclude_expensive_cond);
+COND* substitute_for_best_equal_field(COND *cond, COND_EQUAL *cond_equal, void *table_join_idx);
+bool list_contains_unique_index(Table *table, bool (*find_func) (Field *, void *), void *data);
+bool find_field_in_order_list (Field *field, void *data);
+bool find_field_in_item_list (Field *field, void *data);
+bool test_if_skip_sort_order(JOIN_TAB *tab,order_st *order,ha_rows select_limit, bool no_changes, const key_map *map);
+order_st *create_distinct_group(Session *session,
+                                Item **ref_pointer_array,
+                                order_st *order_list,
+                                List<Item> &fields,
+                                List<Item> &,
+                                bool *all_order_by_fields_used);
+// Create list for using with tempory table
+bool change_to_use_tmp_fields(Session *session,
+                              Item **ref_pointer_array,
+			                        List<Item> &res_selected_fields,
+			                        List<Item> &res_all_fields,
+			                        uint32_t elements,
+                              List<Item> &all_fields);
+int do_select(JOIN *join, List<Item> *fields, Table *tmp_table);
+bool const_expression_in_where(COND *conds,Item *item, Item **comp_item);
+int create_sort_index(Session *session, JOIN *join, order_st *order, ha_rows filesort_limit, ha_rows select_limit, bool is_order_by);
+void advance_sj_state(const table_map remaining_tables, const JOIN_TAB *tab);
+void restore_prev_sj_state(const table_map remaining_tables, const JOIN_TAB *tab);
+void save_index_subquery_explain_info(JOIN_TAB *join_tab, Item* where);
+Item *remove_additional_cond(Item* conds);
+bool setup_sum_funcs(Session *session, Item_sum **func_ptr);
+bool init_sum_functions(Item_sum **func, Item_sum **end);
+bool update_sum_func(Item_sum **func);
+void copy_sum_funcs(Item_sum **func_ptr, Item_sum **end);
+bool change_refs_to_tmp_fields(Session *session,
+                               Item **ref_pointer_array,
+                               List<Item> &res_selected_fields,
+                               List<Item> &res_all_fields,
+                               uint32_t elements,
+			                         List<Item> &all_fields);
+void select_describe(JOIN *join, bool need_tmp_table,bool need_order, bool distinct, const char *message= NULL);
+int subq_sj_candidate_cmp(Item_in_subselect* const *el1, Item_in_subselect* const *el2);
+bool convert_subq_to_sj(JOIN *parent_join, Item_in_subselect *subq_pred);
+bool change_group_ref(Session *session, Item_func *expr, order_st *group_list, bool *changed);
+bool check_interleaving_with_nj(JOIN_TAB *last, JOIN_TAB *next);
+
+int join_read_const_table(JOIN_TAB *tab, POSITION *pos);
+int join_read_system(JOIN_TAB *tab);
+int join_read_const(JOIN_TAB *tab);
+int join_read_key(JOIN_TAB *tab);
+int join_read_always_key(JOIN_TAB *tab);
+int join_read_last_key(JOIN_TAB *tab);
+int join_no_more_records(READ_RECORD *info);
+int join_read_next(READ_RECORD *info);
+int join_read_next_different(READ_RECORD *info);
+int join_init_quick_read_record(JOIN_TAB *tab);
+int test_if_quick_select(JOIN_TAB *tab);
+int join_init_read_record(JOIN_TAB *tab);
+int join_read_first(JOIN_TAB *tab);
+int join_read_next_same(READ_RECORD *info);
+int join_read_next_same_diff(READ_RECORD *info);
+int join_read_last(JOIN_TAB *tab);
+int join_read_prev_same(READ_RECORD *info);
+int join_read_prev(READ_RECORD *info);
+int join_read_always_key_or_null(JOIN_TAB *tab);
+int join_read_next_same_or_null(READ_RECORD *info);
+
+void calc_used_field_length(Session *, JOIN_TAB *join_tab);
+store_key *get_store_key(Session *session, 
+                         KEYUSE *keyuse,
+                         table_map used_tables,
+                         KEY_PART_INFO *key_part,
+                         unsigned char *key_buff,
+                         uint32_t maybe_null);
+extern "C" int join_tab_cmp(const void* ptr1, const void* ptr2);
+extern "C" int join_tab_cmp_straight(const void* ptr1, const void* ptr2);
+void push_index_cond(JOIN_TAB *tab, uint32_t keyno, bool other_tbls_ok);
+void add_not_null_conds(JOIN *join);
+uint32_t max_part_bit(key_part_map bits);
+COND *add_found_match_trig_cond(JOIN_TAB *tab, COND *cond, JOIN_TAB *root_tab);
+int join_init_cache(Session *session,JOIN_TAB *tables,uint32_t table_count);
+order_st *create_distinct_group(Session *session,
+                                Item **ref_pointer_array,
+                                order_st *order,
+                                List<Item> &fields,
+                                List<Item> &all_fields,
+                                bool *all_order_by_fields_used);
+bool eq_ref_table(JOIN *join, order_st *start_order, JOIN_TAB *tab);
+uint64_t get_bound_sj_equalities(TableList *sj_nest, table_map remaining_tables);
+int join_tab_cmp(const void* ptr1, const void* ptr2);
+int remove_dup_with_compare(Session *session, Table *table, Field **first_field, uint32_t offset, Item *having);
+int remove_dup_with_hash_index(Session *session, 
+                               Table *table,
+                               uint32_t field_count,
+                               Field **first_field,
+                               uint32_t key_length,
+                               Item *having);
+bool update_ref_and_keys(Session *session,
+                         DYNAMIC_ARRAY *keyuse,
+                         JOIN_TAB *join_tab,
+                         uint32_t tables,
+                         COND *cond, 
+                         COND_EQUAL *,
+                         table_map normal_tables,
+                         Select_Lex *select_lex,
+                         SARGABLE_PARAM **sargables);
+ha_rows get_quick_record_count(Session *session, SQL_SELECT *select, Table *table, const key_map *keys,ha_rows limit);
+void optimize_keyuse(JOIN *join, DYNAMIC_ARRAY *keyuse_array);
+void add_group_and_distinct_keys(JOIN *join, JOIN_TAB *join_tab);
+int do_sj_reset(SJ_TMP_TABLE *sj_tbl);
+bool store_record_in_cache(JOIN_CACHE *cache);
+void reset_cache_read(JOIN_CACHE *cache);
+void reset_cache_write(JOIN_CACHE *cache);
+void read_cached_record(JOIN_TAB *tab);
+// Create list for using with tempory table
+void init_tmptable_sum_functions(Item_sum **func);
+void update_tmptable_sum_func(Item_sum **func,Table *tmp_table);
+bool find_eq_ref_candidate(Table *table, table_map sj_inner_tables);
+bool only_eq_ref_tables(JOIN *join, order_st *order, table_map tables);
+bool create_ref_for_key(JOIN *join, JOIN_TAB *j, KEYUSE *org_keyuse, table_map used_tables);
 
 /* functions from opt_sum.cc */
 bool simple_pred(Item_func *func_item, Item **args, bool *inv_order);
