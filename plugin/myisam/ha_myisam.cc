73e29edb5550ee5e906f15cd79527f11ab0871cb
Brian Aker <brian@gaz>
2009-06-15 14:05:47 -0700

    Committing Stewart's tmp fix

--- plugin/myisam/ha_myisam.cc
+++ plugin/myisam/ha_myisam.cc
@@ -70,7 +70,7 @@ class MyisamEngine : public StorageEngine
 {
 public:
   MyisamEngine(string name_arg)
-   : StorageEngine(name_arg, HTON_CAN_RECREATE) {}
+   : StorageEngine(name_arg, HTON_CAN_RECREATE | HTON_TEMPORARY_ONLY) {}
 
   virtual handler *create(TableShare *table,
                           MEM_ROOT *mem_root)
