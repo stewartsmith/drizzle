54ff685abd7b0e0170a8b4e932d933f163da71cd
devananda <devananda.vdv@gmail.com>
2009-07-01 21:02:41 -0700

    applied comments from Jay&Stewart reviews: added de-init func, renamed from Item_func_benchmark to BenchmarkFunction, misc other cleanup

--- plugin/benchmark/benchmarkudf.cc
+++ plugin/benchmark/benchmarkudf.cc
@@ -1,6 +1,20 @@
-/* 
- -*- mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; -*-
+/* -*- mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; -*-
  *  vim:expandtab:shiftwidth=2:tabstop=2:smarttab:
+ *
+ * Copyright (C) 2008 MySQL AB
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA 
  */
 
 #include <drizzled/server_includes.h>
@@ -9,36 +23,47 @@
 
 using namespace std;
 
-class Item_func_benchmark :public Item_int_func
+class BenchmarkFunction :public Item_int_func
 {
 public:
-//  Item_func_benchmark(Item *count_expr, Item *expr) :Item_int_func(count_expr, expr)  {}
-  Item_func_benchmark() :Item_int_func() {}
+  BenchmarkFunction() :Item_int_func() {}
   int64_t val_int();
   virtual void print(String *str, enum_query_type query_type);
 
-  const char *func_name() const      { return "benchmark"; }
-  void fix_length_and_dec()          { max_length=1; maybe_null=0; }
-  bool check_argument_count(int n)   { return (n==2); }
+  const char *func_name() const
+  { 
+     return "benchmark"; 
+  }
+
+  void fix_length_and_dec()
+  { 
+     max_length=1; 
+     maybe_null=0;
+  }
+
+  bool check_argument_count(int n)
+  { 
+     return (n==2); 
+  }
 };
 
 
 /* This function is just used to test speed of different functions */
-int64_t Item_func_benchmark::val_int()
+int64_t BenchmarkFunction::val_int()
 {
   assert(fixed == 1);
   char buff[MAX_FIELD_WIDTH];
   String tmp(buff,sizeof(buff), &my_charset_bin);
   my_decimal tmp_decimal;
-  Session *session=current_session;
+  Session *session= current_session;
   uint64_t loop_count;
 
   loop_count= (uint64_t) args[0]->val_int();
 
   if (args[0]->null_value ||
-      (!args[0]->unsigned_flag && (((int64_t) loop_count) < 0)))
+      (args[0]->unsigned_flag == false && (((int64_t) loop_count) < 0)))
   {
-    if (!args[0]->null_value)
+    if (args[0]->null_value == false)
     {
       llstr(((int64_t) loop_count), buff);
       push_warning_printf(current_session, DRIZZLE_ERROR::WARN_LEVEL_ERROR,
@@ -50,10 +75,14 @@ int64_t Item_func_benchmark::val_int()
     return 0;
   }
 
-  null_value=0;
-  for (uint64_t loop=0 ; loop < loop_count && !session->killed; loop++)
+  null_value= 0;
+
+
+  uint64_t loop;
+  for (loop= 0 ; loop < loop_count && !session->killed; loop++)
   {
-    switch (args[1]->result_type()) {
+    switch (args[1]->result_type()) 
+    {
     case REAL_RESULT:
       (void) args[1]->val_real();
       break;
@@ -76,7 +105,7 @@ int64_t Item_func_benchmark::val_int()
   return 0;
 }
 
-void Item_func_benchmark::print(String *str, enum_query_type query_type)
+void BenchmarkFunction::print(String *str, enum_query_type query_type)
 {
   str->append(STRING_WITH_LEN("benchmark("));
   args[0]->print(str, query_type);
@@ -85,12 +114,18 @@ void Item_func_benchmark::print(String *str, enum_query_type query_type)
   str->append(')');
 }
 
-Create_function<Item_func_benchmark> benchmarkudf(string("benchmark"));
+Create_function<BenchmarkFunction> benchmarkudf(string("benchmark"));
 
 static int initialize(PluginRegistry &registry)
 {
   registry.add(&benchmarkudf);
   return 0;
+}
+
+static int finalize(PluginRegistry &registry)
+{
+   registry.remove(&benchmarkudf);
+   return 0;
 }
 
 drizzle_declare_plugin(benchmark)
@@ -101,7 +136,7 @@ drizzle_declare_plugin(benchmark)
   "Measure time for repeated calls to a function.",
   PLUGIN_LICENSE_GPL,
   initialize, /* Plugin Init */
-  NULL,   /* Plugin Deinit */
+  finalize,   /* Plugin Deinit */
   NULL,   /* status variables */
   NULL,   /* system variables */
   NULL    /* config options */
