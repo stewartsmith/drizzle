281b9234a70440c393b54b55bfe695f380d2e633
devananda <devananda.vdv@gmail.com>
2009-07-09 15:19:22 -0700

    formatting cleanup

--- plugin/md5/md5.cc
+++ plugin/md5/md5.cc
@@ -57,38 +57,33 @@ public:
 
 String *Md5Function::val_str(String *str)
 {
-  assert(fixed == 1);
-  String * sptr= args[0]->val_str(str);
-  str->set_charset(&my_charset_bin);
+  assert(fixed == true);
 
-  if (sptr) 
+  String *sptr= args[0]->val_str(str);
+  if (sptr == NULL || str->alloc(32) ) 
   {
-    MD5_CTX context;
-    unsigned char digest[16];
-
-    null_value= 0;
-    MD5_Init   (&context);
-    MD5_Update (&context, (unsigned char *) sptr->ptr(), sptr->length());
-    MD5_Final  (digest, &context);
-
-    if (str->alloc(32))				// Ensure that memory is free
-    {  
-      null_value= 1;
-      return 0;
-    }
-
-    snprintf((char *) str->ptr(), 33,
-	    "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
-	    digest[0], digest[1], digest[2], digest[3],
-	    digest[4], digest[5], digest[6], digest[7],
-	    digest[8], digest[9], digest[10], digest[11],
-	    digest[12], digest[13], digest[14], digest[15]);
-    str->length((uint) 32);
-    return str;
+    null_value= true;
+    return 0;
   }
 
-  null_value= 1;
-  return 0;
+  null_value= false;
+
+  unsigned char digest[16];
+  str->set_charset(&my_charset_bin);
+  MD5_CTX    context;
+  MD5_Init   (&context);
+  MD5_Update (&context, (unsigned char *) sptr->ptr(), sptr->length());
+  MD5_Final  (digest, &context);
+
+  snprintf((char *) str->ptr(), 33,
+    "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+    digest[0], digest[1], digest[2], digest[3],
+    digest[4], digest[5], digest[6], digest[7],
+    digest[8], digest[9], digest[10], digest[11],
+    digest[12], digest[13], digest[14], digest[15]);
+  str->length((uint) 32);
+
+  return str;
 }
 
 
