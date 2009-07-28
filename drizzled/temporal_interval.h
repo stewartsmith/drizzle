8cb57749f667e4debf71ac233a5709f8f7d2a12b
Jay Pipes <jpipes@serialcoder>
2009-07-28 15:16:29 -0400

    Fix indentation of temporal_interval.h per IRC conversation with clint

--- drizzled/temporal_interval.h
+++ drizzled/temporal_interval.h
@@ -3,7 +3,9 @@
  *
  *  Copyright (C) 2009 Sun Microsystems
  *
- *  Author: Clint Byrum <clint@fewbar.com>
+ *  Authors: 
+ *
+ *  Clint Byrum <clint@fewbar.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -20,147 +22,153 @@
  */
 #include <drizzled/item.h>
 
-namespace drizzled {
+namespace drizzled 
+{
+
+/**
+ * @brief
+ *  Stores time interval for date/time manipulation
+ */
+class TemporalInterval
+{
+public:
+
+  TemporalInterval(uint32_t in_year,
+      uint32_t in_month,
+      uint32_t in_day,
+      uint32_t in_hour,
+      uint64_t in_minute,
+      uint64_t in_second,
+      uint64_t in_second_part,
+      bool in_neg)
+    :
+      year(in_year),
+      month(in_month),
+      day(in_day),
+      hour(in_hour),
+      minute(in_minute),
+      second(in_second),
+      second_part(in_second_part),
+      neg(in_neg)
+  {}
+
+  TemporalInterval()
+    :
+      year(0),
+      month(0),
+      day(0),
+      hour(0),
+      minute(0),
+      second(0),
+      second_part(0),
+      neg(false)
+  {}
+
   /**
-   * @brief
-   *  Stores time interval for date/time manipulation
+   * Sets whether or not this object specifies a negative interval
+   * @param[in] in_neg true if this is a negative interval, false if not
    */
-  class TemporalInterval
+  void setNegative(bool in_neg= true)
   {
-    public:
+    neg= in_neg;
+  }
 
-      TemporalInterval(uint32_t in_year,
-          uint32_t in_month,
-          uint32_t in_day,
-          uint32_t in_hour,
-          uint64_t in_minute,
-          uint64_t in_second,
-          uint64_t in_second_part,
-          bool in_neg)
-        :
-          year(in_year),
-          month(in_month),
-          day(in_day),
-          hour(in_hour),
-          minute(in_minute),
-          second(in_second),
-          second_part(in_second_part),
-          neg(in_neg)
-    {}
+  /**
+   * reverse boolean value of the negative flag
+   */
+  void toggleNegative()
+  {
+    neg= !neg;
+  }
 
-      TemporalInterval()
-        :
-          year(0),
-          month(0),
-          day(0),
-          hour(0),
-          minute(0),
-          second(0),
-          second_part(0),
-          neg(false)
-    {}
+  /**
+   * @retval true this is a negative temporal interval
+   * @retval false this is a positive temporal interval
+   */
+  bool getNegative() const
+  {
+    return neg;
+  }
 
-      /**
-       * Sets whether or not this object specifies a negative interval
-       * @param[in] in_neg true if this is a negative interval, false if not
-       */
-      void setNegative(bool in_neg= true)
-      {
-        neg= in_neg;
-      }
+  /**
+   * Populate this TemporalInterval from a string value
+   *
+   * To make code easy, allow interval objects without separators.
+   *
+   * @param args argument Item structure
+   * @param int_type type of interval to create
+   * @param str_value String pointer to the input value
+   * @return true if the string would result in a null interval
+   * 
+   */
+  bool initFromItem(Item *args, interval_type int_type, String *str_value);
 
-      /**
-       * reverse boolean value of the negative flag
-       */
-      void toggleNegative()
-      {
-        neg= !neg;
-      }
+  /**
+   * Adds this interval to a DRIZZLE_LTIME structure
+   *
+   * @param[in,out] ltime the interval will be added to ltime directly in the ltime structure
+   * @param[in] int_type the type of interval requested
+   * @retval true date was added and value stored properly
+   * @retval false result of addition is a null value
+   */
+  bool addDate(DRIZZLE_TIME *ltime, interval_type int_type);
 
-      /**
-       * @retval true this is a negative temporal interval
-       * @retval false this is a positive temporal interval
-       */
-      bool getNegative() const
-      {
-        return neg;
-      }
+private:
 
-      /**
-       * Populate this TemporalInterval from a string value
-       *
-       * To make code easy, allow interval objects without separators.
-       *
-       * @param args argument Item structure
-       * @param int_type type of interval to create
-       * @param str_value String pointer to the input value
-       * @return true if the string would result in a null interval
-       * 
-       */
-      bool initFromItem(Item *args, interval_type int_type, String *str_value);
+  /**
+   * The maximum number of text elements to extract into a temporal interval
+   */
+  static const uint32_t MAX_STRING_ELEMENTS = 5;
 
-      /**
-       * Adds this interval to a DRIZZLE_LTIME structure
-       *
-       * @param[in,out] ltime the interval will be added to ltime directly in the ltime structure
-       * @param[in] int_type the type of interval requested
-       * @retval true date was added and value stored properly
-       * @retval false result of addition is a null value
-       */
-      bool addDate(DRIZZLE_TIME *ltime, interval_type int_type);
+  /**
+   * Each of these corresponds to an 'interval_type'
+   */
+  static const uint32_t NUM_YEAR_MONTH_STRING_ELEMENTS         = 2;
+  static const uint32_t NUM_DAY_HOUR_STRING_ELEMENTS           = 2; 
+  static const uint32_t NUM_DAY_MICROSECOND_STRING_ELEMENTS    = 5;
+  static const uint32_t NUM_DAY_MINUTE_STRING_ELEMENTS         = 3;
+  static const uint32_t NUM_DAY_SECOND_STRING_ELEMENTS         = 4;
+  static const uint32_t NUM_HOUR_MICROSECOND_STRING_ELEMENTS   = 4;
+  static const uint32_t NUM_HOUR_MINUTE_STRING_ELEMENTS        = 2;
+  static const uint32_t NUM_HOUR_SECOND_STRING_ELEMENTS        = 3;
+  static const uint32_t NUM_MINUTE_MICROSECOND_STRING_ELEMENTS = 3;
+  static const uint32_t NUM_MINUTE_SECOND_STRING_ELEMENTS      = 2;
+  static const uint32_t NUM_SECOND_MICROSECOND_STRING_ELEMENTS = 2;
 
-    private:
+  /**
+   *  @details
+   *  Get a array of positive numbers from a string object.
+   *  Each number is separated by 1 non digit character
+   *  Return error if there is too many numbers.
+   *  If there is too few numbers, assume that the numbers are left out
+   *  from the high end. This allows one to give:
+   *  DAY_TO_SECOND as "D MM:HH:SS", "MM:HH:SS" "HH:SS" or as seconds.
+   *
+   *  @param[in] length:         length of str
+   *  @param[in] cs:             charset of str
+   *  @param[out] values:         array of results
+   *  @param[out] count:          count of elements in result array
+   *  @param transform_msec: if value is true we suppose
+   *  that the last part of string value is microseconds
+   *  and we should transform value to six digit value.
+   *  For example, '1.1' -> '1.100000'
+   */
+  bool getIntervalFromString(const char *str,
+                             uint32_t length, 
+                             const CHARSET_INFO * const cs,
+                             uint32_t count, 
+                             uint64_t *values,
+                             bool transform_msec);
 
-      /**
-       * The maximum number of text elements to extract into a temporal interval
-       */
-      static const uint32_t MAX_STRING_ELEMENTS = 5;
+  uint32_t  year;
+  uint32_t  month;
+  uint32_t  day;
+  uint32_t  hour;
+  uint64_t  minute;
+  uint64_t  second;
+  uint64_t  second_part;
+  bool      neg;
 
-      /**
-       * Each of these corresponds to an 'interval_type'
-       */
-      static const uint32_t NUM_YEAR_MONTH_STRING_ELEMENTS         = 2;
-      static const uint32_t NUM_DAY_HOUR_STRING_ELEMENTS           = 2; 
-      static const uint32_t NUM_DAY_MICROSECOND_STRING_ELEMENTS    = 5;
-      static const uint32_t NUM_DAY_MINUTE_STRING_ELEMENTS         = 3;
-      static const uint32_t NUM_DAY_SECOND_STRING_ELEMENTS         = 4;
-      static const uint32_t NUM_HOUR_MICROSECOND_STRING_ELEMENTS   = 4;
-      static const uint32_t NUM_HOUR_MINUTE_STRING_ELEMENTS        = 2;
-      static const uint32_t NUM_HOUR_SECOND_STRING_ELEMENTS        = 3;
-      static const uint32_t NUM_MINUTE_MICROSECOND_STRING_ELEMENTS = 3;
-      static const uint32_t NUM_MINUTE_SECOND_STRING_ELEMENTS      = 2;
-      static const uint32_t NUM_SECOND_MICROSECOND_STRING_ELEMENTS = 2;
+};
 
-      /**
-       *  @details
-       *  Get a array of positive numbers from a string object.
-       *  Each number is separated by 1 non digit character
-       *  Return error if there is too many numbers.
-       *  If there is too few numbers, assume that the numbers are left out
-       *  from the high end. This allows one to give:
-       *  DAY_TO_SECOND as "D MM:HH:SS", "MM:HH:SS" "HH:SS" or as seconds.
-
-       *  @param[in] length:         length of str
-       *  @param[in] cs:             charset of str
-       *  @param[out] values:         array of results
-       *  @param[out] count:          count of elements in result array
-       *  @param transform_msec: if value is true we suppose
-       *  that the last part of string value is microseconds
-       *  and we should transform value to six digit value.
-       *  For example, '1.1' -> '1.100000'
-       */
-      bool getIntervalFromString(const char *str,uint32_t length, const CHARSET_INFO * const cs,
-          uint32_t count, uint64_t *values,
-          bool transform_msec);
-
-      uint32_t  year;
-      uint32_t  month;
-      uint32_t  day;
-      uint32_t  hour;
-      uint64_t  minute;
-      uint64_t  second;
-      uint64_t  second_part;
-      bool      neg;
-
-  };
-}
+} /* end namespace drizzled */
