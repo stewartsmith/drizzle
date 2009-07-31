e932280ca439cfb7d4862b30437bfcca97fbeeb1
Jay Pipes <jpipes@serialcoder>
2009-07-31 15:19:11 -0400

    Adds CRC32 checksumming to Command Log plugin (replication logging)

--- plugin/command_log/command_log.cc
+++ plugin/command_log/command_log.cc
@@ -33,17 +33,20 @@
  * We have an atomic off_t called log_offset which keeps track of the 
  * offset into the log file for writing the next Command.
  *
+ * We write Command message encapsulated in a 8-byte length header and a
+ * 4-byte checksum trailer.
+ *
  * When writing a Command to the log, we calculate the length of the 
  * Command to be written.  We then increment log_offset by the length
- * of the Command plus sizeof(uint64_t) and store this new offset in a 
- * local off_t called cur_offset (see CommandLog::apply().  This 
- * compare and set is done in an atomic instruction.
+ * of the Command plus sizeof(uint64_t) plus sizeof(uint32_t) and store 
+ * this new offset in a local off_t called cur_offset (see CommandLog::apply().  
+ * This compare and set is done in an atomic instruction.
  *
  * We then adjust the local off_t (cur_offset) back to the original
- * offset by subtracting the length and sizeof(uint64_t).
+ * offset by subtracting the length and sizeof(uint64_t) and sizeof(uint32_t).
  *
  * We then first write a 64-bit length and then the serialized transaction/command
- * to our log file at our local cur_offset.
+ * and optional checksum to our log file at our local cur_offset.
  *
  * @todo
  *
@@ -62,6 +65,7 @@
 #include <vector>
 #include <string>
 #include <unistd.h>
+#include <zlib.h>
 
 using namespace std;
 using namespace drizzled;
@@ -77,8 +81,13 @@ static char* sysvar_command_log_file= NULL;
 /** Command Log plugin system variable - A debugging variable to assist in truncating the log file. */
 static bool sysvar_command_log_truncate_debug= false;
 static const char DEFAULT_LOG_FILE_PATH[]= "command.log"; /* In datadir... */
+/** 
+ * Command Log plugin system variable - Should we write a CRC32 checksum for 
+ * each written Command message?
+ */
+static bool sysvar_command_log_checksum_enabled= false;
 
-CommandLog::CommandLog(const char *in_log_file_path)
+CommandLog::CommandLog(const char *in_log_file_path, bool in_do_checksum)
   : 
     plugin::CommandApplier(),
     state(OFFLINE),
@@ -86,6 +95,7 @@ CommandLog::CommandLog(const char *in_log_file_path)
 {
   is_enabled= true; /* If constructed, the plugin is enabled until taken offline with disable() */
   is_active= false;
+  do_checksum= in_do_checksum; /* Have to do here, not in initialization list b/c atomic<> */
 
   /* Setup our log file and determine the next write offset... */
   log_file= open(log_file_path, O_APPEND|O_CREAT|O_SYNC|O_WRONLY, S_IRWXU);
@@ -129,6 +139,9 @@ void CommandLog::apply(const message::Command &to_apply)
    */
   string buffer(""); /* Buffer we will write serialized command to */
 
+  static const uint32_t HEADER_TRAILER_BYTES= sizeof(uint64_t) + /* 8-byte length header */
+                                              sizeof(uint32_t); /* 4 byte checksum trailer */
+
   uint64_t length;
   ssize_t written;
   off_t cur_offset;
@@ -140,14 +153,19 @@ void CommandLog::apply(const message::Command &to_apply)
 
   /*
    * Do an atomic increment on the offset of the log file position
+   *
+   * |<- 8 bytes ->|<- # Bytes of Command Message ->|<- 4 bytes ->|
+   * --------------------------------------------------------------
+   * |   Length    |   Serialized Command Message   |   Checksum  |
+   * --------------------------------------------------------------
    */
-  cur_offset= log_offset.fetch_and_add((off_t) (sizeof(uint64_t) + length));
+  cur_offset= log_offset.fetch_and_add((off_t) (HEADER_TRAILER_BYTES + length));
 
   /*
    * We adjust cur_offset back to the original log_offset before
    * the increment above...
    */
-  cur_offset-= (off_t) (sizeof(uint64_t) + length);
+  cur_offset-= (off_t) (HEADER_TRAILER_BYTES + length);
 
   /* 
    * Quick safety...if an error occurs below, the log file will
@@ -161,11 +179,12 @@ void CommandLog::apply(const message::Command &to_apply)
   unsigned char nbo_length[8];
   int8store(nbo_length, length);
 
+  /* Write the length header */
   do
   {
     written= pwrite(log_file, nbo_length, sizeof(uint64_t), cur_offset);
   }
-  while (written == EINTR); /* Just retry the write when interrupted by a signal... */
+  while (written == -1 && errno == EINTR); /* Just retry the write when interrupted by a signal... */
 
   if (unlikely(written != sizeof(uint64_t)))
   {
@@ -201,11 +220,12 @@ void CommandLog::apply(const message::Command &to_apply)
     return;
   }
 
+  /* Write the command message itself */
   do
   {
     written= pwrite(log_file, buffer.c_str(), length, cur_offset);
   }
-  while (written == EINTR); /* Just retry the write when interrupted by a signal... */
+  while (written == -1 && errno == EINTR); /* Just retry the write when interrupted by a signal... */
 
   if (unlikely(written != (ssize_t) length))
   {
@@ -222,6 +242,58 @@ void CommandLog::apply(const message::Command &to_apply)
      */
     log_offset= cur_offset;
     is_active= false;
+  }
+
+  cur_offset+= (off_t) written;
+
+  /* 
+   * Quick safety...if an error occurs above in another writer, the log 
+   * file will be in a crashed state.
+   */
+  if (unlikely(state == CRASHED))
+  {
+    /* 
+     * Reset the log's offset in case we want to produce a decent error message including
+     * the original offset where an error occurred.
+     */
+    log_offset= cur_offset;
+    return;
+  }
+
+  uint32_t checksum= 0;
+
+  if (do_checksum)
+  {
+    checksum= crc32(0L, (unsigned char *) buffer.c_str(), length);
+  }
+
+  /* We always write in network byte order */
+  unsigned char nbo_checksum[4];
+  int4store(nbo_checksum, checksum);
+
+  /* Write the checksum trailer */
+  do
+  {
+    written= pwrite(log_file, nbo_checksum, sizeof(uint32_t), cur_offset);
+  }
+  while (written == -1 && errno == EINTR); /* Just retry the write when interrupted by a signal... */
+
+  if (unlikely(written != (ssize_t) sizeof(uint32_t)))
+  {
+    errmsg_printf(ERRMSG_LVL_ERROR, 
+                  _("Failed to write full checksum of command.  Tried to write %" PRId64 " bytes at offset %" PRId64 ", but only wrote %" PRId64 " bytes.  Error: %s\n"), 
+                  (int64_t) sizeof(uint32_t), 
+                  (int64_t) cur_offset,
+                  (int64_t) written, 
+                  strerror(errno));
+    state= CRASHED;
+    /* 
+     * Reset the log's offset in case we want to produce a decent error message including
+     * the original offset where an error occurred.
+     */
+    log_offset= cur_offset;
+    is_active= false;
+    return;
   }
 }
 
@@ -270,7 +342,7 @@ static int init(PluginRegistry &registry)
 {
   if (sysvar_command_log_enabled)
   {
-    command_log= new CommandLog(sysvar_command_log_file);
+    command_log= new CommandLog(sysvar_command_log_file, sysvar_command_log_checksum_enabled);
     registry.add(command_log);
   }
   return 0;
@@ -321,10 +393,19 @@ static DRIZZLE_SYSVAR_STR(log_file,
                           NULL, /* update func*/
                           DEFAULT_LOG_FILE_PATH /* default */);
 
+static DRIZZLE_SYSVAR_BOOL(enable_checksum,
+                          sysvar_command_log_checksum_enabled,
+                          PLUGIN_VAR_NOCMDARG,
+                          N_("Enable CRC32 Checksumming"),
+                          NULL, /* check func */
+                          NULL, /* update func */
+                          false /* default */);
+
 static struct st_mysql_sys_var* system_variables[]= {
   DRIZZLE_SYSVAR(enable),
   DRIZZLE_SYSVAR(truncate_debug),
   DRIZZLE_SYSVAR(log_file),
+  DRIZZLE_SYSVAR(enable_checksum),
   NULL
 };
 
