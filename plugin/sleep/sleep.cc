1502bf1cab85e2a6dcac571554d40470533d63de
Patrick Galbraith <patg@patrick-galbraiths-macbook-pro.local>
2009-08-14 22:13:40 -0400

    So, I did say I would get to sleep() on the the plane to Seattle ;)
    * implemented sleep(), this time using a mutex to ensure thread-safe
    * This also takes decimal values
    * Scavenged some code from MySQL's sleep() function

--- plugin/sleep/sleep.cc
+++ plugin/sleep/sleep.cc
@@ -18,14 +18,21 @@
  */
 
 #include <unistd.h>
+#include <time.h>
+
 #include <drizzled/server_includes.h>
 #include <drizzled/sql_udf.h>
+#include <drizzled/session.h>
 #include <drizzled/item/func.h>
+#include <mysys/my_pthread.h>
 #include <drizzled/function/str/strfunc.h>
 
 #include <string>
 
 using namespace std;
+
+/* for thread-safe sleep() */
+static pthread_mutex_t LOCK_sleep;
 
 class Item_func_sleep : public Item_int_func
 {
@@ -56,22 +63,70 @@ public:
 
 int64_t Item_func_sleep::val_int()
 {
-  /* int time in seconds */
-  uint dtime;
-  /* string time */
-  String *stime;
+  int error= 0;
 
-  if ((arg_count != 1) || ! (stime= args[0]->val_str(&value)))
+  /* int time in seconds, decimal allowed */
+  double dtime;
+
+  struct timespec abstime;
+
+  pthread_cond_t cond;
+
+  Session *session= current_session;
+
+  if ((arg_count != 1) || ! (dtime= args[0]->val_real()))
   {
     null_value= true;
     return 0;
   }
 
-  /* obtain int time */
-  dtime= atoi(stime->c_ptr());
+  /*
+    On 64-bit OSX pthread_cond_timedwait() waits forever
+    if passed abstime time has already been exceeded by 
+    the system time.
+    When given a very short timeout (< 10 mcs) just return 
+    immediately.
+    We assume that the lines between this test and the call 
+    to pthread_cond_timedwait() will be executed in less than 0.00001 sec.
+  */
+  if (dtime < 0.00001)
+    return 0;
 
-  /* sleep dtime seconds */
-  sleep(dtime);
+  /* need to obtain time value for passing to cond_timedwait */
+  set_timespec_nsec(abstime, (uint64_t)(dtime * 1000000000ULL));
+
+  /* init cond */
+  pthread_cond_init(&cond, NULL);
+  /* lock */
+  pthread_mutex_lock(&LOCK_sleep);
+  session->set_proc_info("you ahr veddy sleepy!");
+
+  /* set current mutex to the mutex of interest */
+  session->mysys_var->current_mutex= &LOCK_sleep;
+  session->mysys_var->current_cond= &cond;
+
+  /* don't run if not killed */
+  while(!session->killed)
+  {
+    error= pthread_cond_timedwait(&cond, &LOCK_sleep, &abstime);
+    if (error == ETIMEDOUT || error == ETIME)
+      break;
+    null_value= true;
+    error= 0;
+  }
+  /* indicate that we're done */
+  session->set_proc_info(0);
+  /* unlock */
+  pthread_mutex_unlock(&LOCK_sleep);
+
+  /* now, we need to clean up, set back to zero */
+  pthread_mutex_lock(&session->mysys_var->mutex);
+  session->mysys_var->current_mutex= 0;
+  session->mysys_var->current_cond= 0;
+  pthread_mutex_unlock(&session->mysys_var->mutex);
+
+  /* relenquish pthread cond */
+  pthread_cond_destroy(&cond);
 
   null_value= false;
   return 0;
@@ -83,6 +138,8 @@ Create_function<Item_func_sleep>
 static int sleep_plugin_init(PluginRegistry &registry)
 {
   registry.add(&sleep_udf);
+  /* is this the right place for this? */
+  pthread_mutex_init(&LOCK_sleep, MY_MUTEX_INIT_FAST);
   return 0;
 }
 
