950b60d3922336ff8dac1b3c49ed0a4a46ae07f9
Monty Taylor <mordred@inaugust.com>
2009-08-27 11:40:07 -0400

    Added missing copyright headers. Added drizzled/global.h to a few things that were missing it. Added a couple of more excludes from clean_source.sh.

--- drizzled/message/ioutil.h
+++ drizzled/message/ioutil.h
@@ -1,9 +1,31 @@
-// -*- Mode: C++ -*-
+/* -*- mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; -*-
+ *  vim:expandtab:shiftwidth=2:tabstop=2:smarttab:
+ *
+ *  Copyright (C) 2009 Sun Microsystems
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 #include <iosfwd>
 #include <string>
 
-namespace ioutil {
+namespace drizzled
+{
+namespace message
+{
+namespace ioutil
+{
 
   /**
      Helper class for join() I/O manipulator.
@@ -64,4 +86,7 @@ namespace ioutil {
     typedef typename Container::const_iterator FwdIter;
     return joiner<FwdIter>(delim, seq.begin(), seq.end());
   }
-}
+
+} /* namespace ioutil */
+} /* namespace message */
+} /* namespace drizzled */
