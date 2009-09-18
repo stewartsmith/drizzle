0178767036634624866c5975c3ec4d8495ffae0b
Jay Pipes <jpipes@serialcoder>
2009-09-18 13:15:07 -0400

    Adds an error catch in heap_create_table which traps when a call to create a HEAP table with more than UINT32_MAX occurs.  This allows us to safely use a cast without worrying about truncation.

--- plugin/heap/ha_heap.cc
+++ plugin/heap/ha_heap.cc
@@ -670,6 +670,16 @@ int HeapEngine::heap_create_table(Session *session, const char *table_name,
   TableShare *share= table_arg->s;
   bool found_real_auto_increment= 0;
 
+  /* 
+   * We cannot create tables with more rows than UINT32_MAX.  This is a
+   * limitation of the HEAP engine.  Here, since TableShare::getMaxRows()
+   * can return a number more than that, we trap it here instead of casting
+   * to a truncated integer.
+   */
+  uint64_t num_rows= share->getMaxRows();
+  if (num_rows > UINT32_MAX)
+    return -1;
+
   if (!(columndef= (HP_COLUMNDEF*) malloc(column_count * sizeof(HP_COLUMNDEF))))
     return my_errno;
 
@@ -822,14 +832,16 @@ int HeapEngine::heap_create_table(Session *session, const char *table_name,
   hp_create_info.internal_table= internal_table;
   hp_create_info.max_chunk_size= share->block_size;
   hp_create_info.is_dynamic= (share->row_type == ROW_TYPE_DYNAMIC);
+
   error= heap_create(fn_format(buff,table_name,"","",
-                               MY_REPLACE_EXT|MY_UNPACK_FILENAME),
-                     keys, keydef,
-                     column_count, columndef,
-                     max_key_fieldnr, key_part_size,
-                     share->reclength, mem_per_row_keys,
-                     (uint32_t)share->getMaxRows(), 0, // Factor out MIN
-                     &hp_create_info, internal_share);
+                              MY_REPLACE_EXT|MY_UNPACK_FILENAME),
+                    keys, keydef,
+                    column_count, columndef,
+                    max_key_fieldnr, key_part_size,
+                    share->reclength, mem_per_row_keys,
+                    static_cast<uint32_t>(num_rows), /* We check for overflow above, so cast is fine here. */
+                    0, // Factor out MIN
+                    &hp_create_info, internal_share);
 
   free((unsigned char*) keydef);
   free((void *) columndef);
