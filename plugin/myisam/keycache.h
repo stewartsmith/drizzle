7b13e74ea2022d33148ea5e9637875b8d3e48ae4
Stewart Smith <stewart@flamingspork.com>
2009-10-08 23:49:35 +1100

    remove dead prototype for multi_key_cache_set()

--- plugin/myisam/keycache.h
+++ plugin/myisam/keycache.h
@@ -149,8 +149,6 @@ extern void end_key_cache(KEY_CACHE *keycache, bool cleanup);
 /* Functions to handle multiple key caches */
 extern bool multi_keycache_init(void);
 extern void multi_keycache_free(void);
-extern bool multi_key_cache_set(const unsigned char *key, uint32_t length,
-				   KEY_CACHE *key_cache);
 extern void reset_key_cache_counters();
 
 #ifdef __cplusplus
