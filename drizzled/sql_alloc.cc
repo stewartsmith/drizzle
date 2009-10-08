0b24b864306982aa5ae1c8c86be7cea75ba1bb2d
Stewart Smith <stewart@flamingspork.com>
2009-10-09 02:02:59 +1100

    SunStudioa needs extern C, and gcc needs it with prototype (sql_alloc.cc)

--- drizzled/sql_alloc.cc
+++ drizzled/sql_alloc.cc
@@ -20,6 +20,8 @@
 #include <drizzled/current_session.h>
 #include <drizzled/error.h>
 
+extern "C" void sql_alloc_error_handler(void);
+
 extern "C" void sql_alloc_error_handler(void)
 {
   errmsg_printf(ERRMSG_LVL_ERROR, "%s",ER(ER_OUT_OF_RESOURCES));
