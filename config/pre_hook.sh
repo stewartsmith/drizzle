f4a6aef742cc3485e8db9de639d4041fd6224bd6
Monty Taylor <mordred@inaugust.com>
2009-11-03 13:23:44 -0800

    pandora-build v0.67 - Support configure-time bzr version checking.

--- config/pre_hook.sh
+++ config/pre_hook.sh
@@ -1,44 +1,5 @@
 # pre_hook.sh - Commands that we run before we run the autotools
 
-RELEASE_DATE=`date +%Y.%m`
-RELEASE_DATE_NODOTS=`date +%Y%m`
-if test -d ".bzr" ; then
-  echo "Grabbing changelog and version information from bzr"
-  bzr log --short > ChangeLog || touch ChangeLog
-  BZR_REVNO=`bzr revno`
-  BZR_REVID=`bzr log -r-1 --show-ids | grep revision-id | awk '{print $2}' | head -1`
-  BZR_BRANCH=`bzr nick`
-else
-  touch ChangeLog
-  BZR_REVNO="0"
-  BZR_REVID="unknown"
-  BZR_BRANCH="bzr-export"
-fi
-RELEASE_VERSION="${RELEASE_DATE}.${BZR_REVNO}"
-RELEASE_ID="${RELEASE_DATE_NODOTS}${BZR_REVNO}"
-if test "x${BZR_BRANCH}" != "xdrizzle" ; then
-  RELEASE_COMMENT="${BZR_BRANCH}"
-else
-  RELEASE_COMMENT="trunk"
-fi
-
-if test -f m4/bzr_version.m4.in
-then
-  sed -e "s/@BZR_REVNO@/${BZR_REVNO}/" \
-      -e "s/@BZR_REVID@/${BZR_REVID}/" \
-      -e "s/@BZR_BRANCH@/${BZR_BRANCH}/" \
-      -e "s/@RELEASE_DATE@/${RELEASE_DATE}/" \
-      -e "s/@RELEASE_ID@/${RELEASE_ID}/" \
-      -e "s/@RELEASE_VERSION@/${RELEASE_VERSION}/" \
-      -e "s/@RELEASE_COMMENT@/${RELEASE_COMMENT}/" \
-    m4/bzr_version.m4.in > m4/bzr_version.m4.new
-  
-  if diff m4/bzr_version.m4.new m4/bzr_version.m4 >/dev/null 2>&1 ; then
-    rm m4/bzr_version.m4.new
-  else
-    mv m4/bzr_version.m4.new m4/bzr_version.m4
-  fi
-fi
 
 EGREP=`which egrep`
 if test "x$EGREP" != "x" -a -d po
