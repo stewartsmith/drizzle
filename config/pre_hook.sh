0cad685c5e790b7e841d8f0f9d5f695979cc795d
Monty Taylor <mordred@inaugust.com>
2009-11-14 13:17:54 -0800

    It seems to work.

--- config/pre_hook.sh
+++ config/pre_hook.sh
@@ -3,7 +3,4 @@ if test ! -f  config/plugin.stamp
 then
   touch config/plugin.stamp
 fi
-python config/register_plugins.py plugin.list
-python config/register_plugins.py plugin-list.am
-python config/register_plugins.py plugin-list.ac
-python config/register_plugins.py . .
+python config/register_plugins.py write
