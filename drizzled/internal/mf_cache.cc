cf9d23b29170bc58cadcdf637963438405cd8372
Tim Penhey <tim@penhey.net>
2010-01-22 15:55:38 +1300

    Make it compile.

--- drizzled/internal/mf_cache.cc
+++ drizzled/internal/mf_cache.cc
@@ -15,11 +15,11 @@
 
 /* Open a temporary file and cache it with io_cache. Delete it on close */
 
-#include "drizzled/error.h"
 #include "drizzled/internal/mysys_priv.h"
 #include "drizzled/internal/m_string.h"
 #include "drizzled/internal/my_static.h"
 #include "drizzled/internal/iocache.h"
+#include "drizzled/error.h"
 
 
 	/*
