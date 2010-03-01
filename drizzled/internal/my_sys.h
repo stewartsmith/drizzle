64226390f8dec4a088ff5ada6e37f2f4aa907c2a
Stewart Smith <stewart@flamingspork.com>
2010-03-01 12:23:07 +1100

    remove now unused mysys #define ME_HIGHBYTE.

--- drizzled/internal/my_sys.h
+++ drizzled/internal/my_sys.h
@@ -101,7 +101,6 @@ namespace internal
 #define MY_DONT_OVERWRITE_FILE 1024	/* my_copy: Don't overwrite file */
 #define MY_THREADSAFE 2048      /* my_seek(): lock fd mutex */
 
-#define ME_HIGHBYTE	8	/* Shift for colours */
 #define ME_OLDWIN	2	/* Use old window */
 #define ME_BELL		4	/* Ring bell then printing message */
 #define ME_HOLDTANG	8	/* Don't delete last keys */
