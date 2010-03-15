9705d63f12601d8f78697934d2ad79fd5dab2ac3
Stewart Smith <stewart@flamingspork.com>
2010-03-15 15:31:10 +1100

    simple table function for Embedded InnoDB Configuration. Currently returns nothing.

---
+++ plugin/embedded_innodb/config_table_function.cc
@@ -0,0 +1,87 @@
+/*
+  Copyright (C) 2010 Stewart Smith
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include "config.h"
+#include "drizzled/plugin/table_function.h"
+
+#include "config_table_function.h"
+
+using namespace std;
+using namespace drizzled;
+
+class LibInnoDBConfigTool : public drizzled::plugin::TableFunction
+{
+public:
+
+  LibInnoDBConfigTool();
+
+  LibInnoDBConfigTool(const char *table_arg) :
+    drizzled::plugin::TableFunction("data_dictionary", table_arg)
+  { }
+
+  ~LibInnoDBConfigTool() {}
+
+  class Generator : public drizzled::plugin::TableFunction::Generator
+  {
+  public:
+    Generator(drizzled::Field **arg);
+
+    bool populate();
+  };
+
+  LibInnoDBConfigTool::Generator *generator(drizzled::Field **arg)
+  {
+    return new Generator(arg);
+  }
+};
+
+LibInnoDBConfigTool::LibInnoDBConfigTool() :
+  plugin::TableFunction("DATA_DICTIONARY", "INNODB_CONFIGURATION")
+{
+  add_field("NAME");
+  add_field("TYPE");
+  add_field("VALUE");
+}
+
+LibInnoDBConfigTool::Generator::Generator(Field **arg) :
+  plugin::TableFunction::Generator(arg)
+{
+}
+
+bool LibInnoDBConfigTool::Generator::populate()
+{
+  return false; // No more rows
+}
+
+static LibInnoDBConfigTool *config_tool;
+
+int config_table_function_initialize(drizzled::plugin::Registry &registry)
+{
+  config_tool= new(std::nothrow)LibInnoDBConfigTool();
+  registry.add(config_tool);
+
+  return 0;
+}
+
+int config_table_function_finalize(drizzled::plugin::Registry &registry)
+{
+  registry.remove(config_tool);
+  delete config_tool;
+
+  return 0;
+}
