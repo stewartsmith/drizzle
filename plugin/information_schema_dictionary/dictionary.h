44302dc0e65d8cee5fc8b4f219f46cac578abe66
Brian Aker <brian@gaz>
2010-03-17 16:02:16 -0700

    Make check catch

--- plugin/information_schema_dictionary/dictionary.h
+++ plugin/information_schema_dictionary/dictionary.h
@@ -18,8 +18,8 @@
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#ifndef PLUGIN_INFORMATION_SCHEMA_DICTIONARY_H
-#define PLUGIN_INFORMATION_SCHEMA_DICTIONARY_H
+#ifndef PLUGIN_INFORMATION_SCHEMA_DICTIONARY_DICTIONARY_H
+#define PLUGIN_INFORMATION_SCHEMA_DICTIONARY_DICTIONARY_H
 
 #include <set>
 
@@ -49,4 +49,4 @@
 #include "plugin/information_schema_dictionary/view_table_usage.h"
 #include "plugin/information_schema_dictionary/views.h"
 
-#endif /* PLUGIN_INFORMATION_SCHEMA_DICTIONARY_H */
+#endif /* PLUGIN_INFORMATION_SCHEMA_DICTIONARY_DICTIONARY_H */
