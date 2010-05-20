3b08c0f6f6ef1e560a5c9aaca4210ad8bbe1a49a
Zimin <ziminq@gmail.com>
2010-05-20 20:10:51 +0800

    merge from fs-plugin

---
+++ plugin/filesystem/ha_filesystem.cc
@@ -0,0 +1,519 @@
+/* -*- mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; -*-
+ *  vim:expandtab:shiftwidth=2:tabstop=2:smarttab:
+ *
+ *  Copyright (C) 2010 ziminq
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "config.h"
+#include <drizzled/field.h>
+#include <drizzled/field/blob.h>
+#include <drizzled/field/timestamp.h>
+#include <drizzled/error.h>
+#include <drizzled/table.h>
+#include <drizzled/session.h>
+#include "drizzled/internal/my_sys.h"
+#include <google/protobuf/io/zero_copy_stream.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+
+#include "ha_filesystem.h"
+
+#include <fcntl.h>
+
+#include <string>
+#include <map>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+using namespace drizzled;
+
+/* Stuff for shares */
+pthread_mutex_t filesystem_mutex;
+
+static const char *ha_filesystem_exts[] = {
+  NULL
+};
+
+class FilesystemEngine : public drizzled::plugin::StorageEngine
+{
+private:
+  typedef std::map<string, FilesystemShare*> FilesystemMap;
+  FilesystemMap fs_open_tables;
+public:
+  FilesystemEngine(const string& name_arg)
+   : drizzled::plugin::StorageEngine(name_arg,
+                                     HTON_NULL_IN_KEY |
+                                     HTON_CAN_INDEX_BLOBS |
+                                     HTON_SKIP_STORE_LOCK |
+                                     HTON_AUTO_PART_KEY),
+     fs_open_tables()
+  {
+    table_definition_ext = ".FST";  // should we set this first?
+  }
+  virtual ~FilesystemEngine()
+  {
+    pthread_mutex_destroy(&filesystem_mutex);
+  }
+
+  virtual Cursor *create(TableShare &table,
+                         drizzled::memory::Root *mem_root)
+  {
+    return new (mem_root) ha_filesystem(*this, table);
+  }
+
+  const char **bas_ext() const {
+    return ha_filesystem_exts;
+  }
+
+  int doCreateTable(Session &,
+                    Table &table_arg,
+                    drizzled::TableIdentifier &identifier,
+                    drizzled::message::Table&);
+
+  int doGetTableDefinition(Session& ,
+                           TableIdentifier &,
+                           drizzled::message::Table &);
+
+  /* Temp only engine, so do not return values. */
+  void doGetTableNames(drizzled::CachedDirectory &, drizzled::SchemaIdentifier &, drizzled::plugin::TableNameList &);
+
+  int doDropTable(Session&, TableIdentifier &);
+
+  /* operations on FilesystemShare */
+  FilesystemShare *findOpenTable(const string table_name);
+  void addOpenTable(const string &table_name, FilesystemShare *);
+  void deleteOpenTable(const string &table_name);
+
+  uint32_t max_keys()          const { return 0; }
+  uint32_t max_key_parts()     const { return 0; }
+  uint32_t max_key_length()    const { return 0; }
+  bool doDoesTableExist(Session& , TableIdentifier &);
+  int doRenameTable(Session&, TableIdentifier &, TableIdentifier &) { return false;}
+  void doGetTableIdentifiers(drizzled::CachedDirectory &directory,
+                             drizzled::SchemaIdentifier &schema_identifier,
+                             drizzled::TableIdentifiers &set_of_identifiers);
+};
+
+void FilesystemEngine::doGetTableNames(drizzled::CachedDirectory &directory,
+                                       drizzled::SchemaIdentifier &, /* database name? */
+                                       drizzled::plugin::TableNameList &set_of_names)
+{
+  drizzled::CachedDirectory::Entries entries= directory.getEntries();
+
+  for (drizzled::CachedDirectory::Entries::iterator entry_iter= entries.begin();
+      entry_iter != entries.end(); ++entry_iter)
+  {
+    drizzled::CachedDirectory::Entry *entry= *entry_iter;
+    const string *filename= &entry->filename;
+
+    assert(filename->size());
+
+    const char *ext= strchr(filename->c_str(), '.');
+
+    if (ext == NULL || my_strcasecmp(system_charset_info, ext, ".FST") ||
+        (filename->compare(0, strlen(TMP_FILE_PREFIX), TMP_FILE_PREFIX) == 0))
+    {  }
+    else
+    {
+      char uname[NAME_LEN + 1];
+      uint32_t file_name_len;
+
+      file_name_len= filename_to_tablename(filename->c_str(), uname, sizeof(uname));
+      uname[file_name_len - sizeof(".FST") + 1]= '\0'; // Subtract ending, place NULL
+      set_of_names.insert(uname);
+    }
+  }
+}
+
+int FilesystemEngine::doDropTable(Session &, TableIdentifier &identifier)
+{
+  string new_path(identifier.getPath());
+  new_path+= ".FST";
+  int err= unlink(new_path.c_str());
+  if (err)
+  {
+    err= errno;
+  }
+  return err;
+}
+
+bool FilesystemEngine::doDoesTableExist(Session &, TableIdentifier &identifier)
+{
+  string proto_path(identifier.getPath());
+  proto_path.append(".FST");
+
+  if (access(proto_path.c_str(), F_OK))
+  {
+    return false;
+  }
+
+  return true;
+}
+
+FilesystemShare *FilesystemEngine::findOpenTable(const string table_name)
+{
+  FilesystemMap::iterator find_iter=
+    fs_open_tables.find(table_name);
+
+  if (find_iter != fs_open_tables.end())
+    return (*find_iter).second;
+  else
+    return NULL;
+}
+
+void FilesystemEngine::addOpenTable(const string &table_name, FilesystemShare *share)
+{
+  fs_open_tables[table_name]= share;
+}
+
+void FilesystemEngine::deleteOpenTable(const string &table_name)
+{
+  fs_open_tables.erase(table_name);
+}
+
+int FilesystemEngine::doGetTableDefinition(Session &,
+                               drizzled::TableIdentifier &identifier,
+                               drizzled::message::Table &table_proto)
+{
+  string new_path;
+
+  new_path= identifier.getPath();
+  new_path+= ".FST";
+
+  int fd= open(new_path.c_str(), O_RDONLY);
+
+  if (fd == -1)
+  {
+    cerr << "can't open!" << endl;
+    return errno;
+  }
+
+  google::protobuf::io::ZeroCopyInputStream* input=
+    new google::protobuf::io::FileInputStream(fd);
+
+  if (not input) {
+    return HA_ERR_CRASHED_ON_USAGE;
+  }
+
+  if (not table_proto.ParseFromZeroCopyStream(input))
+  {
+    close(fd);
+    delete input;
+    if (not table_proto.IsInitialized())
+    {
+      my_error(ER_CORRUPT_TABLE_DEFINITION, MYF(0),
+               table_proto.InitializationErrorString().c_str());
+      return ER_CORRUPT_TABLE_DEFINITION;
+    }
+
+    return HA_ERR_CRASHED_ON_USAGE;
+  }
+
+  delete input;
+
+  return EEXIST;
+}
+
+void FilesystemEngine::doGetTableIdentifiers(drizzled::CachedDirectory &directory,
+                           drizzled::SchemaIdentifier &schema_identifier,
+                           drizzled::TableIdentifiers &set_of_identifiers)
+{
+  drizzled::CachedDirectory::Entries entries= directory.getEntries();
+
+  for (drizzled::CachedDirectory::Entries::iterator entry_iter= entries.begin();
+       entry_iter != entries.end(); ++entry_iter)
+  {
+    drizzled::CachedDirectory::Entry *entry= *entry_iter;
+    const string *filename= &entry->filename;
+
+    assert(filename->size());
+
+    const char *ext= strchr(filename->c_str(), '.');
+
+    if (ext == NULL || my_strcasecmp(system_charset_info, ext, ".FST") ||
+        (filename->compare(0, strlen(TMP_FILE_PREFIX), TMP_FILE_PREFIX) == 0))
+    {  }
+    else
+    {
+      char uname[NAME_LEN + 1];
+      uint32_t file_name_len;
+
+      file_name_len= filename_to_tablename(filename->c_str(), uname, sizeof(uname));
+      uname[file_name_len - sizeof(".FST") + 1]= '\0'; // Subtract ending, place NULL
+      set_of_identifiers.push_back(TableIdentifier(schema_identifier, uname));
+    }
+  }
+}
+
+FilesystemShare::FilesystemShare(const string table_name_arg)
+  : use_count(0), table_name(table_name_arg)
+{
+  thr_lock_init(&lock);
+}
+
+FilesystemShare::~FilesystemShare()
+{
+  thr_lock_delete(&lock);
+  pthread_mutex_destroy(&mutex);
+}
+
+/*
+  Simple lock controls.
+*/
+FilesystemShare *ha_filesystem::get_share(const char *table_name)
+{
+  pthread_mutex_lock(&filesystem_mutex);
+
+  FilesystemEngine *a_engine= static_cast<FilesystemEngine *>(engine);
+  share= a_engine->findOpenTable(table_name);
+
+  /*
+    If share is not present in the hash, create a new share and
+    initialize its members.
+  */
+  if (share == NULL)
+  {
+    share= new (nothrow) FilesystemShare(table_name);
+    if (share == NULL)
+    {
+      pthread_mutex_unlock(&filesystem_mutex);
+      return NULL;
+    }
+
+    a_engine->addOpenTable(share->table_name, share);
+
+    // initialize FormatInfo if necessary
+    // initialize per share mutex
+    pthread_mutex_init(&share->mutex,MY_MUTEX_INIT_FAST);
+  }
+  share->use_count++;
+  pthread_mutex_unlock(&filesystem_mutex);
+
+  return share;
+}
+
+ha_filesystem::ha_filesystem(drizzled::plugin::StorageEngine &engine_arg, TableShare &table_arg)
+  :Cursor(engine_arg, table_arg)
+{
+}
+
+/*
+  Open a database file. Keep in mind that tables are caches, so
+  this will not be called for every request. Any sort of positions
+  that need to be reset should be kept in the ::extra() call.
+*/
+int ha_filesystem::open(const char *name, int, uint32_t)
+{
+  if (!(share= get_share(name)))
+    return HA_ERR_OUT_OF_MEM; // TODO: code style???
+
+  thr_lock_data_init(&share->lock, &lock, NULL);
+  return 0;
+}
+
+/*
+  Close a database file. We remove ourselves from the shared strucutre.
+  If it is empty we destroy it.
+*/
+int ha_filesystem::close(void)
+{
+  return 0;
+}
+
+/*
+  All table scans call this first.
+  The order of a table scan is:
+
+  ha_tina::info
+  ha_tina::rnd_init
+  ha_tina::extra
+  ENUM HA_EXTRA_CACHE   Cash record in HA_rrnd()
+  ha_tina::rnd_next
+  ha_tina::rnd_next
+  ha_tina::rnd_next
+  ha_tina::rnd_next
+  ha_tina::rnd_next
+  ha_tina::rnd_next
+  ha_tina::rnd_next
+  ha_tina::rnd_next
+  ha_tina::rnd_next
+  ha_tina::extra
+  ENUM HA_EXTRA_NO_CACHE   End cacheing of records (def)
+  ha_tina::extra
+  ENUM HA_EXTRA_RESET   Reset database to after open
+
+  Each call to ::rnd_next() represents a row returned in the can. When no more
+  rows can be returned, rnd_next() returns a value of HA_ERR_END_OF_FILE.
+  The ::info() call is just for the optimizer.
+
+*/
+
+int ha_filesystem::doStartTableScan(bool)
+{
+  // open a specific file
+  fd.open("demo.txt");
+  if (!fd.is_open())
+    cerr << "can't open demo.txt file." << endl;
+  return 0;
+}
+
+/*
+  ::rnd_next() does all the heavy lifting for a table scan. You will need to
+  populate *buf with the correct field data. You can walk the field to
+  determine at what position you should store the data (take a look at how
+  ::find_current_row() works). The structure is something like:
+  0Foo  Dog  Friend
+  The first offset is for the first attribute. All space before that is
+  reserved for null count.
+  Basically this works as a mask for which rows are nulled (compared to just
+  empty).
+  This table Cursor doesn't do nulls and does not know the difference between
+  NULL and "". This is ok since this table Cursor is for spreadsheets and
+  they don't know about them either :)
+*/
+int ha_filesystem::rnd_next(unsigned char *buf)
+{
+  drizzled::String buffer;
+  string line;
+
+  if (!fd.is_open())
+    return HA_ERR_END_OF_FILE;
+
+  if (!getline(fd, line))
+    return HA_ERR_END_OF_FILE;
+
+  memset(buf, 0, table->s->null_bytes); //getNullBytes()
+  istringstream is(line);
+  for (Field **field = table->field; *field; ++field)
+  {
+    buffer.length(0);
+    string word;
+    if (is >> word)
+    {
+      cerr << "field: " << word << endl;
+      buffer.append(word.c_str());
+      if ((*field)->isReadSet() || (*field)->isWriteSet())
+      {
+        // this is very important to use 'select',
+        // as said in csv file, it's a bug in select ???
+        (*field)->setWriteSet();
+        (*field)->store(buffer.ptr()/* pointer */,
+                        buffer.length()/* length */,
+                        buffer.charset()/* charset */,
+                        CHECK_FIELD_WARN/* check flag */);
+      }
+    }
+  }
+  return 0;
+}
+
+/*
+  In the case of an order by rows will need to be sorted.
+  ::position() is called after each call to ::rnd_next(),
+  the data it stores is to a byte array. You can store this
+  data via my_store_ptr(). ref_length is a variable defined to the
+  class that is the sizeof() of position being stored. In our case
+  its just a position. Look at the bdb code if you want to see a case
+  where something other then a number is stored.
+*/
+void ha_filesystem::position(const unsigned char *)
+{
+  return;
+}
+
+
+/*
+  Used to fetch a row from a posiion stored with ::position().
+  internal::my_get_ptr() retrieves the data for you.
+*/
+
+int ha_filesystem::rnd_pos(unsigned char * buf, unsigned char *pos)
+{
+  (void)buf;
+  (void)pos;
+  return 0;
+}
+
+/*
+  ::info() is used to return information to the optimizer.
+  Currently this table Cursor doesn't implement most of the fields
+  really needed. SHOW also makes use of this data
+*/
+int ha_filesystem::info(uint32_t)
+{
+  return 0;
+}
+
+int ha_filesystem::doEndTableScan()
+{
+  fd.close();
+  return 0;
+}
+
+/*
+  Create a table. You do not want to leave the table open after a call to
+  this (the database will call ::open() if it needs to).
+*/
+int FilesystemEngine::doCreateTable(Session &,
+                        Table&,
+                        drizzled::TableIdentifier &identifier,
+                        drizzled::message::Table &proto)
+{
+  string serialized_proto;
+  string new_path;
+
+  new_path= identifier.getPath();
+  new_path+= ".FST";
+  fstream output(new_path.c_str(), ios::out | ios::binary);
+
+  if (! output)
+    return 1;
+
+  if (! proto.SerializeToOstream(&output))
+  {
+    output.close();
+    unlink(new_path.c_str());
+    return 1;
+  }
+
+  return 0;
+}
+
+static FilesystemEngine *filesystem_engine= NULL;
+
+static int filesystem_init_func(drizzled::plugin::Context &context)
+{
+  filesystem_engine = new FilesystemEngine("FILESYSTEM");
+  context.add(filesystem_engine);
+
+  pthread_mutex_init(&filesystem_mutex, MY_MUTEX_INIT_FAST);
+  return 0;
+}
+
+DRIZZLE_DECLARE_PLUGIN
+{
+  DRIZZLE_VERSION_ID,
+  "FILESYSTEM",
+  "1.0",
+  "ziminq",
+  "Filesystem Engine",
+  PLUGIN_LICENSE_GPL,
+  filesystem_init_func, /* Plugin Init */
+  NULL,                       /* system variables                */
+  NULL                        /* config options                  */
+}
+DRIZZLE_DECLARE_PLUGIN_END;
---
+++ plugin/filesystem/ha_filesystem.h
@@ -0,0 +1,88 @@
+/* -*- mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; -*-
+ *  vim:expandtab:shiftwidth=2:tabstop=2:smarttab:
+ *
+ *  Copyright (C) 2010 ziminq
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef PLUGIN_FILESYSTEM_HA_FILESYSTEM_H
+#define PLUGIN_FILESYSTEM_HA_FILESYSTEM_H
+
+#include <drizzled/cursor.h>
+#include <drizzled/thr_lock.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fstream>
+using namespace std;
+
+class FilesystemShare
+{
+  FilesystemShare();
+  FilesystemShare(const FilesystemShare &);
+  FilesystemShare& operator=(const FilesystemShare &);
+public:
+  explicit FilesystemShare(const std::string name);
+  ~FilesystemShare();
+
+  uint32_t use_count;
+  const std::string table_name;
+  pthread_mutex_t mutex;
+  drizzled::THR_LOCK lock;
+};
+
+class ha_filesystem : public drizzled::Cursor
+{
+  drizzled::THR_LOCK_DATA lock;      /* MySQL lock */
+  FilesystemShare *share;       /* Shared lock info */
+  std::ifstream fd;
+
+public:
+  ha_filesystem(drizzled::plugin::StorageEngine &engine, drizzled::TableShare &table_arg);
+  ~ha_filesystem()
+  {
+  }
+
+  /** @brief
+    The name that will be used for display purposes.
+   */
+  const char *table_type(void) const { return "FILESYSTEM"; }
+
+  /*
+     Called in test_quick_select to determine if indexes should be used.
+   */
+  virtual double scan_time() { return (double) (stats.records+stats.deleted) / 20.0+10; }
+
+  /* The next method will never be called */
+  virtual bool fast_key_read() { return 1;}
+  drizzled::ha_rows estimate_rows_upper_bound() { return HA_POS_ERROR; }
+
+  int open(const char *name, int mode, uint32_t open_options);
+  int close(void);
+  int doStartTableScan(bool scan=1);
+  int rnd_next(unsigned char *);
+  int rnd_pos(unsigned char * , unsigned char *);
+  void position(const unsigned char *);
+  int info(uint);
+  int doEndTableScan();
+
+  virtual void get_auto_increment(uint64_t offset, uint64_t increment,
+                                  uint64_t nb_desired_values,
+                                  uint64_t *first_value,
+                                  uint64_t *nb_reserved_values) { (void)offset; (void)increment; (void)nb_desired_values; (void)first_value; (void)nb_reserved_values; };
+  FilesystemShare *get_share(const char *table_name);
+};
+
+#endif /* PLUGIN_FILESYSTEM_HA_FILESYSTEM_H */
