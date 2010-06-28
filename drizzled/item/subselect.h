a8662acaeac6dac2f702d346099ab76acd681a29
Prafulla Tekawade <prafulla_t@users.sourceforge.net>
2010-06-28 22:25:23 +0530

    Fix for Bug 592473

--- drizzled/item/subselect.h
+++ drizzled/item/subselect.h
@@ -320,6 +320,10 @@ public:
   };
   enum_exec_method exec_method;
 
+  Item* get_left_expr() const  {
+    return left_expr;
+  }
+
   bool *get_cond_guard(int i)
   {
     return pushed_cond_guards ? pushed_cond_guards + i : NULL;
@@ -329,7 +333,15 @@ public:
     if ( pushed_cond_guards)
       pushed_cond_guards[i]= v;
   }
-  bool have_guarded_conds() { return test(pushed_cond_guards); }
+  bool have_guarded_conds() {
+    if( NULL == pushed_cond_guards )
+      return false;
+    uint32_t nCol = get_left_expr()->cols();
+    bool bCondGuardOn = false;
+    for( uint32_t uColIdx = 0; uColIdx < nCol; uColIdx++)
+      bCondGuardOn |= pushed_cond_guards[uColIdx];
+    return bCondGuardOn;
+  }
 
   Item_func_not_all *upper_item; // point on NOT/NOP before ALL/SOME subquery
 
