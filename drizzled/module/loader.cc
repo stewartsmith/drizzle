8996d4322d8fe01c150e111b5fd4f74cc4d2ebce
LinuxJedi <linuxjedi@linuxjedi-laptop>
2010-09-02 22:47:37 +0100

    Make --print-defaults ack as old behaviour for the defaults list at the end of --help because this is still needed in drizzled for dtr.  Killing 2 birds with one stone.

--- drizzled/module/loader.cc
+++ drizzled/module/loader.cc
@@ -79,6 +79,8 @@ char opt_plugin_dir[FN_REFLEN];
 const char *opt_plugin_load_default= PANDORA_PLUGIN_LIST;
 const char *builtin_plugins= PANDORA_BUILTIN_LIST;
 
+extern bool opt_print_defaults;
+
 /* Note that 'int version' must be the first field of every plugin
    sub-structure (plugin->info).
 */
@@ -1820,8 +1822,15 @@ void my_print_help_inc_plugins(option *main_options,
   /* main_options now points to the empty option terminator */
   all_options.push_back(*main_options);
 
-  my_print_help(&*(all_options.begin()));
-  cout << long_options << endl;
+  if (!opt_print_defaults)
+  {
+    my_print_help(&*(all_options.begin()));
+    cout << long_options << endl;
+  }
+  else
+  {
+    my_print_variables(&*(all_options.begin()));
+  }
 
   mem_root.free_root(MYF(0));
 }
