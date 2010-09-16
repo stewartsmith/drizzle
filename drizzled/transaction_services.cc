bc8ac4b4b9081abd74703b6c3b5b9ee4b29e93b8
Joseph Daly <jdaly@rx7>
2010-09-15 21:32:10 -0400

    fix up bugs 638518, and memory leak problems

--- drizzled/transaction_services.cc
+++ drizzled/transaction_services.cc
@@ -822,31 +822,23 @@ int TransactionServices::rollbackToSavepoint(Session *session, NamedSavepoint &s
   if (shouldConstructMessages())
   {
     cleanupTransactionMessage(getActiveTransactionMessage(session), session);
-    message::Transaction *savepoint_transaction= sv.getTransactionSavepoint();
-
-    google::protobuf::RepeatedPtrField< message::Statement> *statements= 
-      savepoint_transaction->mutable_statement();
-
-    /* A iterator is used here rather then the other GPB functions as there needs
-       to be a check for the case where there are no statements (a NULL value) */
-    if (statements != NULL)
+    message::Transaction *savepoint_transaction= sv.getTransactionMessage();
+    if (savepoint_transaction != NULL)
     {
-      google::protobuf::RepeatedPtrField< message::Statement>::iterator it= 
-        statements->begin();
-      google::protobuf::RepeatedPtrField< message::Statement>::iterator end_it= 
-        statements->end();
- 
-      message::Statement *new_statement= NULL;
-      
-      for (; it != end_it; ++it)
+      message::Transaction *savepoint_transaction_copy= new message::Transaction(*sv.getTransactionMessage());
+      uint32_t num_statements = savepoint_transaction->statement_size();
+      if (num_statements == 0)
+      {    
+        session->setStatementMessage(NULL);
+      }    
+      else 
       {
-        new_statement= &*it;
-      }
-
-      session->setTransactionMessage(savepoint_transaction);
-      session->setStatementMessage(new_statement);
+        session->setStatementMessage(savepoint_transaction_copy->mutable_statement(num_statements - 1));    
+      }    
+      session->setTransactionMessage(savepoint_transaction_copy);
     }
   }
+
   return error;
 }
 
@@ -900,7 +892,7 @@ int TransactionServices::setSavepoint(Session *session, NamedSavepoint &sv)
     {
       message::Transaction *transaction_savepoint= 
         new message::Transaction(*transaction);
-      sv.setTransactionSavepoint(transaction_savepoint);
+      sv.setTransactionMessage(transaction_savepoint);
     }
   } 
 
@@ -932,12 +924,6 @@ int TransactionServices::releaseSavepoint(Session *session, NamedSavepoint &sv)
     }
   }
   
-  if (shouldConstructMessages())
-  {
-    delete sv.getTransactionSavepoint();
-    sv.setTransactionSavepoint(NULL);
-  }
-
   return error;
 }
 
