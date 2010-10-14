a28cbae36d64b8e134d8bbc84d0dc2d1e5faa839
Vijay Samuel <vjsamuel1990@gmail.com>
2010-10-14 23:53:33 +0530

    Merge corrections to licensing information.

--- client/thread_context.h
+++ client/thread_context.h
@@ -2,6 +2,7 @@
  *  vim:expandtab:shiftwidth=2:tabstop=2:smarttab:
  *
  *  Copyright (C) 2010 Vijay Samuel
+ *  Copyright (C) 2008 MySQL
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
