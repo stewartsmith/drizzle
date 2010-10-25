e43f45158abd8b0fcab760155ac8a3e086374e18
Stewart Smith <stewart@flamingspork.com>
2010-10-25 15:23:28 +1100

    update state transitions in Storage Engine API tester to match reality (well, mostly). The series of calls is rather screwy. also implement Cursor states and enough to run a simple SELECT statement.

--- plugin/storage_engine_api_tester/storage_engine_api_tester.cc
+++ plugin/storage_engine_api_tester/storage_engine_api_tester.cc
@@ -34,12 +34,17 @@ using namespace std;
 using namespace drizzled;
 
 typedef boost::unordered_map<std::string, message::Table, util::insensitive_hash, util::insensitive_equal_to> TableMessageMap;
+typedef boost::unordered_map<std::string, message::Table, util::insensitive_hash, util::insensitive_equal_to>::iterator TableMessageMapIterator;
 
 string engine_state;
 typedef multimap<string, string> state_multimap;
 typedef multimap<string, string>::value_type state_pair;
 typedef multimap<string, string>::iterator state_multimap_iter;
 state_multimap engine_state_transitions;
+state_multimap cursor_state_transitions;
+
+/* This is a hack to make store_lock kinda work */
+drizzled::THR_LOCK share_lock;
 
 static inline void ENGINE_STATE(const string &state)
 {
@@ -93,18 +98,106 @@ public:
   SEAPITesterCursor(drizzled::plugin::StorageEngine &engine_arg,
                     drizzled::TableShare &table_arg)
     : Cursor(engine_arg, table_arg)
-    {}
+    { cursor_state= "Cursor()"; }
   ~SEAPITesterCursor()
   {}
 
   int close() { return 0; }
-  int rnd_next(unsigned char*) { return -1; }
-  int rnd_pos(unsigned char*, unsigned char*) { return -1; }
-  void position(const unsigned char*) { return; }
+  int rnd_next(unsigned char*) { CURSOR_NEW_STATE("::rnd_next()"); return HA_ERR_END_OF_FILE; }
+  int rnd_pos(unsigned char*, unsigned char*) { CURSOR_NEW_STATE("::rnd_pos()"); return -1; }
+  void position(const unsigned char*) { CURSOR_NEW_STATE("::position()"); return; }
   int info(uint32_t) { return 0; }
   void get_auto_increment(uint64_t, uint64_t, uint64_t, uint64_t*, uint64_t*) {}
-  int doStartTableScan(bool) { return -1; }
+  int doStartTableScan(bool) { CURSOR_NEW_STATE("::doStartTableScan()"); return HA_ERR_END_OF_FILE; }
+
+  int open(const char*, int, uint32_t)
+    { CURSOR_NEW_STATE("::open()"); lock.init(&share_lock); return 0;}
+
+  drizzled::THR_LOCK_DATA lock;        /* MySQL lock */
+
+  THR_LOCK_DATA **store_lock(Session *,
+                                     THR_LOCK_DATA **to,
+                             enum thr_lock_type);
+
+
+private:
+  string cursor_state;
+  void CURSOR_NEW_STATE(const string &new_state);
 };
+
+THR_LOCK_DATA **SEAPITesterCursor::store_lock(Session *session,
+                                              THR_LOCK_DATA **to,
+                                              enum thr_lock_type lock_type)
+
+{
+  CURSOR_NEW_STATE("::store_lock()");
+
+  if (lock_type != TL_IGNORE && lock.type == TL_UNLOCK)
+  {
+    /*
+      Here is where we get into the guts of a row level lock.
+      If TL_UNLOCK is set
+      If we are not doing a LOCK Table or DISCARD/IMPORT
+      TABLESPACE, then allow multiple writers
+    */
+
+    if ((lock_type >= TL_WRITE_CONCURRENT_INSERT &&
+         lock_type <= TL_WRITE)
+        && !session_tablespace_op(session))
+      lock_type = TL_WRITE_ALLOW_WRITE;
+
+    /*
+      In queries of type INSERT INTO t1 SELECT ... FROM t2 ...
+      MySQL would use the lock TL_READ_NO_INSERT on t2, and that
+      would conflict with TL_WRITE_ALLOW_WRITE, blocking all inserts
+      to t2. Convert the lock to a normal read lock to allow
+      concurrent inserts to t2.
+    */
+
+    if (lock_type == TL_READ_NO_INSERT)
+      lock_type = TL_READ;
+
+    lock.type=lock_type;
+  }
+
+  *to++= &lock;
+
+  return(to);
+}
+
+void SEAPITesterCursor::CURSOR_NEW_STATE(const string &new_state)
+{
+  state_multimap_iter cur= cursor_state_transitions.find(cursor_state);
+  if (cursor_state_transitions.count(cursor_state) == 0)
+  {
+    cerr << "ERROR: Invalid Cursor state: " << cursor_state << endl
+         << "This should *NEVER* happen."
+         << endl
+         << "i.e. you've really screwed it up and you should be ashamed of "
+         << "yourself." << endl;
+    assert(cursor_state_transitions.count(cursor_state));
+  }
+
+  for(cur= cursor_state_transitions.lower_bound(cursor_state);
+      cur != cursor_state_transitions.upper_bound(cursor_state);
+      cur++)
+  {
+    if (new_state.compare((*cur).second) == 0)
+      break;
+  }
+
+  if (cur == cursor_state_transitions.end()
+      || new_state.compare((*cur).second))
+  {
+    cerr << "ERROR: Invalid Cursor state transition!" << endl
+         << "Cannot go from " << cursor_state << " to " << new_state << endl;
+    assert(false);
+  }
+
+  cursor_state= new_state;
+
+  cerr << "\t\tCursor STATE : " << cursor_state << endl;
+}
 
 static const char *api_tester_exts[] = {
   NULL
@@ -116,6 +209,7 @@ class SEAPITester : public drizzled::plugin::TransactionalStorageEngine
 public:
   SEAPITester(const string &name_arg)
     : drizzled::plugin::TransactionalStorageEngine(name_arg,
+//                                                   HTON_SKIP_STORE_LOCK |
                                                    HTON_HAS_DOES_TRANSACTIONS)
   {
     ENGINE_NEW_STATE("::SEAPITester()");
@@ -132,7 +226,6 @@ public:
 
   virtual Cursor *create(TableShare &table)
   {
-    ENGINE_STATE("::SEAPITester()");
     return new SEAPITesterCursor(*this, table);
   }
 
@@ -150,15 +243,13 @@ public:
 
   int doGetTableDefinition(Session& ,
                            const TableIdentifier &,
-                           drizzled::message::Table &)
-    { return ENOENT; }
+                           drizzled::message::Table &);
 
   bool doDoesTableExist(Session&, const TableIdentifier &identifier);
 
   void doGetTableIdentifiers(drizzled::CachedDirectory &,
                              const drizzled::SchemaIdentifier &,
-                             drizzled::TableIdentifiers &)
-     {}
+                             drizzled::TableIdentifiers &);
 
   virtual int doStartTransaction(Session *session,
                                  start_transaction_option_t options);
@@ -189,6 +280,12 @@ bool SEAPITester::doDoesTableExist(Session&, const TableIdentifier &identifier)
   return table_messages.find(identifier.getPath()) != table_messages.end();
 }
 
+void SEAPITester::doGetTableIdentifiers(drizzled::CachedDirectory &,
+                                        const drizzled::SchemaIdentifier &,
+                                        drizzled::TableIdentifiers &)
+{
+}
+
 int SEAPITester::doCreateTable(Session&,
                                Table&,
                                const drizzled::TableIdentifier &identifier,
@@ -217,6 +314,24 @@ int SEAPITester::doDropTable(Session&, const TableIdentifier &identifier)
   table_messages.erase(identifier.getPath());
   assert(table_messages.find(identifier.getPath()) == table_messages.end());
   return 0;
+}
+
+int SEAPITester::doGetTableDefinition(Session& ,
+                                      const TableIdentifier &identifier,
+                                      drizzled::message::Table &table)
+{
+  ENGINE_STATE("::doGetTableDefinition()");
+  TableMessageMapIterator iter= table_messages.find(identifier.getPath());
+  if (iter == table_messages.end())
+  {
+    ENGINE_STATE("::SEAPITester()");
+    return ENOENT;
+  }
+
+  table= (*iter).second;
+
+  ENGINE_STATE("::SEAPITester()");
+  return EEXIST;
 }
 
 int SEAPITester::doStartTransaction(Session *,
@@ -257,8 +372,16 @@ static int seapi_tester_init(drizzled::module::Context &context)
   engine_state_transitions.insert(state_pair("::SEAPITester()", "::~SEAPITester()"));
   engine_state_transitions.insert(state_pair("::SEAPITester()", "::doCreateTable()"));
   engine_state_transitions.insert(state_pair("::doCreateTable()", "::SEAPITester()"));
+/*  engine_state_transitions.insert(state_pair("::SEAPITester()", "::create()"));
+    engine_state_transitions.insert(state_pair("::create()", "::SEAPITester()"));*/
   engine_state_transitions.insert(state_pair("::SEAPITester()", "BEGIN"));
   engine_state_transitions.insert(state_pair("BEGIN", "START STATEMENT"));
+
+  /* really a bug */
+  engine_state_transitions.insert(state_pair("BEGIN", "END STATEMENT"));
+  /* also a bug */
+  engine_state_transitions.insert(state_pair("::SEAPITester()", "COMMIT"));
+
   engine_state_transitions.insert(state_pair("BEGIN", "COMMIT"));
   engine_state_transitions.insert(state_pair("BEGIN", "ROLLBACK"));
   engine_state_transitions.insert(state_pair("START STATEMENT", "END STATEMENT"));
@@ -268,7 +391,22 @@ static int seapi_tester_init(drizzled::module::Context &context)
 
   engine_state_transitions.insert(state_pair("COMMIT", "::SEAPITester()"));
   engine_state_transitions.insert(state_pair("ROLLBACK", "::SEAPITester()"));
+  engine_state_transitions.insert(state_pair("::SEAPITester()", "::doGetTableDefinition()"));
+  engine_state_transitions.insert(state_pair("::doGetTableDefinition()", "::SEAPITester()"));
   engine_state= "INIT";
+
+
+  cursor_state_transitions.insert(state_pair("Cursor()", "::open()"));
+  cursor_state_transitions.insert(state_pair("::open()", "::store_lock()"));
+  cursor_state_transitions.insert(state_pair("::store_lock()", "::doStartTableScan()"));
+  cursor_state_transitions.insert(state_pair("::open()", "::doStartTableScan()"));
+  cursor_state_transitions.insert(state_pair("::doStartTableScan()", "::rnd_next()"));
+
+
+  /* below just for autocommit statement. doesn't seem right to me */
+  engine_state_transitions.insert(state_pair("::SEAPITester()", "START STATEMENT"));
+
+  thr_lock_init(&share_lock); /* HACK for store_lock */
 
   context.add(new SEAPITester(engine_name));
   return 0;
