d20583db9962cdf8efe0ea4648f19a070621fc2e
tdavies <tdavies@Molly>
2010-10-30 10:01:36 -0700

    struct converted to class

--- drizzled/order.h
+++ drizzled/order.h
@@ -29,22 +29,39 @@ class Item;
 
 /* Order clause list element */
 
-struct order_st {
+/* Order clause list element */
+class order_st{
+public:
   struct order_st *next;
-  Item	 **item;			/* Point at item in select fields */
-  Item	 *item_ptr;			/* Storage for initial item */
-  Item   **item_copy;			/* For SPs; the original item ptr */
-  int    counter;                       /* position in SELECT list, correct
-                                           only if counter_used is true*/
-  bool	 asc;				/* true if ascending */
-  bool	 free_me;			/* true if item isn't shared  */
-  bool	 in_field_list;			/* true if in select field list */
-  bool   counter_used;                  /* parameter was counter of columns */
-  Field  *field;			/* If tmp-table group */
-  char	 *buff;				/* If tmp-table group */
+  Item   **item;          /* Point at item in select fields */
+  Item   *item_ptr;       /* Storage for initial item */
+  Item   **item_copy;     /* For SPs; the original item ptr */
+  int    counter;         /* position in SELECT list, correct
+                             only if counter_used is true*/
+  bool   asc;             /* true if ascending */
+  bool   free_me;         /* true if item isn't shared  */
+  bool   in_field_list;   /* true if in select field list */
+  bool   counter_used;    /* parameter was counter of columns */
+  Field  *field;          /* If tmp-table group */
+  char   *buff;           /* If tmp-table group */
   table_map used, depend_map;
-};
 
+  order_st():
+          next(NULL),
+          item(NULL),
+          item_ptr(NULL),
+          item_copy(NULL),
+          counter(0),
+          asc(false),
+          free_me(false),
+          in_field_list(false),
+          counter_used(false),
+          field(NULL),
+          buff(NULL),
+          used(0),
+          depend_map(0)
+  {}
+};
 } /* namespace drizzled */
 
 #endif /* DRIZZLED_ORDER_H */
