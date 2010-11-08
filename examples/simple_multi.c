53cb35c0fb89c8e15672dff832791907c29ec0ff
Stewart Smith <stewart@flamingspork.com>
2010-11-08 18:34:19 +1100

    fix large stack ussage of examples/simple_multi.c: examples/simple_multi.c: In function ‘main’: examples/simple_multi.c:96: error: the frame size of 368544 bytes is larger than 32768 bytes [-Wframe-larger-than=]

--- examples/simple_multi.c
+++ examples/simple_multi.c
@@ -19,12 +19,16 @@ int main(int argc, char *argv[])
 {
   const char *query= "SELECT table_schema,table_name FROM tables";
   drizzle_st drizzle;
-  drizzle_con_st con[SIMPLE_MULTI_COUNT];
-  drizzle_result_st result[SIMPLE_MULTI_COUNT];
-  drizzle_query_st ql[SIMPLE_MULTI_COUNT];
+  drizzle_con_st *con[SIMPLE_MULTI_COUNT];
+  drizzle_result_st *result[SIMPLE_MULTI_COUNT];
+  drizzle_query_st *ql[SIMPLE_MULTI_COUNT];
   drizzle_return_t ret;
   drizzle_row_t row;
   int x;
+
+  con= (drizzle_con_st)malloc(sizeof(drizzle_con_st) * SIMPLE_MULTI_COUNT);
+  result= (drizzle_result_st)malloc(sizeof(drizzle_result_st) * SIMPLE_MULTI_COUNT);
+  ql= (drizzle_query_st)malloc(sizeof(drizzle_query_st) * SIMPLE_MULTI_COUNT);
 
   if (drizzle_create(&drizzle) == NULL)
   {
@@ -92,5 +96,8 @@ int main(int argc, char *argv[])
 
   drizzle_free(&drizzle);
 
+  free(con);
+  free(result);
+  free(ql);
   return 0;
 }
