4fc8230b796fbd2b23737df2c1ab5100e8e53e5a
Brian Aker <brian@tangent.org>
2010-11-26 18:45:19 -0800

    Adding "HAS_GLOBAL_LOCK" to processlist.

--- plugin/session_dictionary/processlist.cc
+++ plugin/session_dictionary/processlist.cc
@@ -46,6 +46,7 @@ ProcesslistTool::ProcesslistTool() :
   add_field("TIME", plugin::TableFunction::NUMBER, 0, false);
   add_field("STATE", plugin::TableFunction::STRING, 256, true);
   add_field("INFO", plugin::TableFunction::STRING, PROCESS_LIST_WIDTH, true);
+  add_field("HAS_GLOBAL_LOCK", plugin::TableFunction::BOOLEAN, PROCESS_LIST_WIDTH, false);
 }
 
 ProcesslistTool::Generator::Generator(Field **arg) :
@@ -117,6 +118,9 @@ bool ProcesslistTool::Generator::populate()
 
     /* INFO */
     push(*tmp->getQueryString());
+
+    /* HAS_GLOBAL_LOCK */
+    push(static_cast<bool>(tmp->isGlobalReadLock()));
 
     return true;
   }
