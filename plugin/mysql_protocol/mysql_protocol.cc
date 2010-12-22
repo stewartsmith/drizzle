c1ba39d3a2e55b80e8553aed1dfd32a0be33f4eb
Brian Aker <brian@tangent.org>
2010-12-22 11:43:00 -0800

    Update for protocol tweak/unittest update.

--- plugin/mysql_protocol/mysql_protocol.cc
+++ plugin/mysql_protocol/mysql_protocol.cc
@@ -531,6 +531,10 @@ bool ClientMySQLProtocol::sendFields(List<Item> *list)
         pos[6]= 12;
         break;
 
+      case DRIZZLE_TYPE_TIME:
+        pos[6]= 13;
+        break;
+
       case DRIZZLE_TYPE_DATE:
         pos[6]= 14;
         break;
@@ -540,10 +544,6 @@ bool ClientMySQLProtocol::sendFields(List<Item> *list)
         break;
 
       case DRIZZLE_TYPE_UUID:
-        pos[6]= 15;
-        break;
-
-      case DRIZZLE_TYPE_TIME:
         pos[6]= 15;
         break;
 
