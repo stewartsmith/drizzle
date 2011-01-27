ba888a0e12f9a0b8c1c95a642a2d3bdc411accbe
Patrick Crews <gleebix@gmail.com>
2011-01-27 10:58:22 -0500

    Added licensing text to dbqp files

--- tests/lib/sys_mgmt/logging_management.py
+++ tests/lib/sys_mgmt/logging_management.py
@@ -3,12 +3,30 @@
 # vim:expandtab:shiftwidth=2:tabstop=2:smarttab:
 #
 # Copyright (C) 2009 Sun Microsystems
+# Copyright (C) 2011 Patrick Crews
 #
 # Authors:
 #
 #  Jay Pipes <joinfu@sun.com>
 #  Monty Taylor <mordred@sun.com>
+#  Patrick Crews 
 #
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; version 2 of the License.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+#
+# This code is modified from the logging module used in the 
+# drizzle-automation project - https://launchpad.net/drizzle-automation
 
 
 """ Simple replacement for python logging module that doesn't suck """
