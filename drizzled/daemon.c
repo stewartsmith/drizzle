7042372b34c3d751edd3d4d3be422576fa0ec20f
Stewart Smith <stewart@flamingspork.com>
2011-02-02 13:49:45 +1100

    update daemon.c copyright

--- drizzled/daemon.c
+++ drizzled/daemon.c
@@ -3,6 +3,8 @@
 /*-
  * Copyright (c) 1990, 1993
  *    The Regents of the University of California.  All rights reserved.
+ * Copyright (c) 2010
+ *    Stewart Smith
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
