73df3adf8132619d4cb156d94ca879e278892966
Patrick Crews <gleebix@gmail.com>
2011-02-03 21:13:52 -0500

    Revert patch for lp:709653.  The patch causes the help menu to be displayed every time the program is run.  Need to reopen the bug / determine if we want to fix this

--- tests/lib/test_run_options.py
+++ tests/lib/test_run_options.py
@@ -328,10 +328,6 @@ debugger_control_group.add_option(
 
 parser.add_option_group(debugger_control_group)
 
-#prints entire help menu when usage message is displayed
-parser.usage= parser.print_help()
-
-
 # supplied will be those arguments matching an option, 
 # and test_cases will be everything else
 (args, test_cases)= parser.parse_args()
