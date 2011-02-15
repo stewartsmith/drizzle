a88bb70c3f4a6103c10fa0b5c36df1501f458715
Stewart Smith <stewart@flamingspork.com>
2011-02-16 10:58:00 +1100

    remove unused PRECISION_FOR_DOUBLE and PRECISION_FOR_FLOAT defines

--- drizzled/definitions.h
+++ drizzled/definitions.h
@@ -183,9 +183,6 @@ const uint32_t MAX_KEY_LENGTH_DECIMAL_WIDTH = 4; // strlen("4096")
 /** Characters shown for the command in 'show processlist'. */
 #define PROCESS_LIST_WIDTH 100
 
-#define PRECISION_FOR_DOUBLE 53
-#define PRECISION_FOR_FLOAT  24
-
 /* The following can also be changed from the command line */
 #define DEFAULT_CONCURRENCY	10
 #define FLUSH_TIME		0		/**< Don't flush tables */
