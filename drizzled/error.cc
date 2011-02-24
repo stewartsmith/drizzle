824719783752e7f7f7c1ef6b03c1eba23fb1add1
Stewart Smith <stewart@flamingspork.com>
2011-02-24 18:46:16 +1100

    add ER_NO_LOCK_HELD error code, and expect it in case of UNLOCK TABLES without any locks held.

--- drizzled/error.cc
+++ drizzled/error.cc
@@ -677,7 +677,7 @@ ErrorMap::ErrorMap()
   ADD_ERROR_MESSAGE(ER_WRONG_NAME_FOR_CATALOG, N_("Invalid catalog name."));
   ADD_ERROR_MESSAGE(ER_USE_DATA_DICTIONARY, N_("Engine status is now stored in the data_dictionary tables, please use these instead."));
   ADD_ERROR_MESSAGE(ER_TRANSACTION_ALREADY_STARTED, N_("There is already a transaction in progress"));
-
+  ADD_ERROR_MESSAGE(ER_NO_LOCK_HELD, N_("No lock is held by this connection."));
 }
 
 } /* namespace drizzled */
