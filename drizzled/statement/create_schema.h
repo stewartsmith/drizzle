bb59e07b1fecc6e84b9ac21f6f92c7b71597d73e
Olaf van der Spek <olafvdspek@gmail.com>
2011-03-08 16:08:53 +0100

    Statement::set_command

--- drizzled/statement/create_schema.h
+++ drizzled/statement/create_schema.h
@@ -41,7 +41,7 @@ public:
     Statement(in_session),
     is_if_not_exists(false)
   {
-    getSession()->getLex()->sql_command=SQLCOM_CREATE_DB;
+    set_command(SQLCOM_CREATE_DB);
   }
 
   bool execute();
