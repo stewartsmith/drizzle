257970dfee101acf440c3f66c47fff4b0d7a3124
Patrick Crews <gleebix@gmail.com>
2011-03-12 21:33:57 -0500

    Added top_builddir/libdrizzle/.libs to LD_LIBRARY_PATH

--- tests/lib/sys_mgmt/codeTree.py
+++ tests/lib/sys_mgmt/codeTree.py
@@ -61,6 +61,7 @@ class drizzleTree(codeTree):
         self.basedir = self.system_manager.find_path([os.path.abspath(variables['basedir'])])
         self.source_dist = os.path.isdir(os.path.join(self.basedir, 'drizzled'))
         self.builddir = self.system_manager.find_path([os.path.abspath(self.basedir)])
+        self.top_builddir = variables['topbuilddir']
         self.testdir = self.system_manager.find_path([os.path.abspath(variables['testdir'])])
         self.clientbindir = self.system_manager.find_path([os.path.join(self.builddir, 'client')
                                      , os.path.join(self.basedir, 'client')
@@ -156,6 +157,7 @@ class drizzleTree(codeTree):
                                , os.path.join(self.basedir,"mysys/.libs/")
                                , os.path.join(self.basedir,"mystrings/.libs/")
                                , os.path.join(self.basedir,"drizzled/.libs/")
+                               , os.path.join(self.top_builddir,"/libdrizzle/.libs")
 			                         , "/usr/local/lib"
                                ]
         else:
