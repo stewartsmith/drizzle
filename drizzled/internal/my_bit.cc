2ddf6e32f5fdd9e08098cc4328f7b9d13fe4bed5
Stewart Smith <stewart@flamingspork.com>
2011-03-18 16:15:04 +1100

    my_bit.cc actually needs no include files

--- drizzled/internal/my_bit.cc
+++ drizzled/internal/my_bit.cc
@@ -13,11 +13,6 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA */
 
-#include <config.h>
-#include <drizzled/internal/my_sys.h>
-
-#include <drizzled/internal/my_bit.h>
-
 namespace drizzled
 {
 namespace internal
