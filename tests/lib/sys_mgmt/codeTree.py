db08972423bd7e8c3a1f0e13bb917bcfb30defda
Patrick Crews <gleebix@gmail.com>
2011-03-23 06:57:09 -0400

    Fixed ld_lib_paths in dbqp to find the new libdrizzle locations in Monty's libdrizzle2.0 tree

--- tests/lib/sys_mgmt/codeTree.py
+++ tests/lib/sys_mgmt/codeTree.py
@@ -155,6 +155,8 @@ class drizzleTree(codeTree):
         if self.source_dist:
             ld_lib_paths = [ os.path.join(self.basedir,"libdrizzleclient/.libs/")
                            , os.path.join(self.basedir,"libdrizzle/.libs")
+                           , os.path.join(self.basedir,"libdrizzle-2.0/libdrizzle/.libs")
+                           , os.path.join(self.basedir,"libdrizzle-1.0/libdrizzle/.libs")
                            , os.path.join(self.basedir,"mysys/.libs/")
                            , os.path.join(self.basedir,"mystrings/.libs/")
                            , os.path.join(self.basedir,"drizzled/.libs/")
