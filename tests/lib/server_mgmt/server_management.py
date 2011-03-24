c2555b52daf71a2d5ed863b3f83a4751c8bdc877
Patrick Crews <gleebix@gmail.com>
2011-03-24 11:47:09 -0400

    Fix to server_management / pid code

--- tests/lib/server_mgmt/server_management.py
+++ tests/lib/server_mgmt/server_management.py
@@ -206,10 +206,11 @@ class serverManager:
      
         if server_retcode == 0:
             server.status = 1 # we are running
-            pid_file = open(server.pid_file,'r')
-            pid = pid_file.readline().strip()
-            pid_file.close()
-            server.pid = pid
+            if os.path.exists(server.pid_file):
+                pid_file = open(server.pid_file,'r')
+                pid = pid_file.readline().strip()
+                pid_file.close()
+                server.pid = pid
 
         if server_retcode != 0 and not expect_fail and self.debug:
             self.logging.debug("Server startup command: %s failed with error code %d" %( start_cmd
