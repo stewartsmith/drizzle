80c7ea51a3e3e11e5c79a93a36de293c5225a531
Stewart Smith <stewart@flamingspork.com>
2011-03-28 15:46:02 +1100

    explicitly #include <string> for xtrabackup

--- plugin/innobase/xtrabackup/xtrabackup.cc
+++ plugin/innobase/xtrabackup/xtrabackup.cc
@@ -27,6 +27,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #endif
 
 #include <config.h>
+#include <string>
 #include <drizzled/internal/my_sys.h>
 #include <drizzled/charset_info.h>
 #include <drizzled/charset.h>
