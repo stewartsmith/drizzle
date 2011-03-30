42db20797a160453f75559c3888dae9289cf2244
Stewart Smith <stewart@flamingspork.com>
2011-03-30 15:14:41 +1100

    definition of error::level_t has moved, fix xtrabackup build

--- plugin/innobase/xtrabackup/xtrabackup.cc
+++ plugin/innobase/xtrabackup/xtrabackup.cc
@@ -35,6 +35,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #include <drizzled/gettext.h>
 #include <drizzled/constrained_value.h>
 #include <drizzled/configmake.h>
+#include <drizzled/error/level_t.h>
 
 #include "ha_prototypes.h"
  //#define XTRABACKUP_TARGET_IS_PLUGIN
