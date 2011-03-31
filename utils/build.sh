7f6eb072cac8f7dedf8747932a8828d776971e8f
Stewart Smith <stewart@flamingspork.com>
2011-03-31 17:36:06 +1100

    add missing copyright headers to shell scripts (Drizzle build farm does a check for correct licenses in all possible source files)

--- utils/build.sh
+++ utils/build.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+# build.sh - build xtrabackup binary against specified InnoDB Flavour
+# Copyright (C) 2009-2011 Percona Inc.
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 MYSQL_51_VERSION=5.1.55
 MYSQL_55_VERSION=5.5.9
 
