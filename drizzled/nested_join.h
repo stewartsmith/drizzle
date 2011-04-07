1da90bd234ed99a710c00b545c5e8dd00a3f12a0
kaaveeacs <kaaveeacs@gmail.com>
2011-04-07 23:43:38 +0400

    Merge privatized 3 members of NestedJoin class

--- drizzled/nested_join.h
+++ drizzled/nested_join.h
@@ -30,12 +30,27 @@ namespace drizzled {
 class NestedJoin
 {
 public:
+  /*
+    This constructor serves for creation of NestedJoin instances
+  */
+  NestedJoin() :
+    join_list(),
+    used_tables(),
+    not_null_tables(),
+    first_nested(NULL),
+    counter_(0),
+    nj_map(),
+    sj_depends_on(),
+    sj_corr_tables(),
+    sj_outer_expr_list()      
+  { }    
+
   /* list of elements in the nested join */
   List<TableList> join_list;
 
   /* bitmap of tables in the nested join */
   table_map used_tables;
-
+  
   /* tables that rejects nulls           */
   table_map not_null_tables;
 
@@ -49,17 +64,20 @@ public:
     by the join optimizer.
     Before each use the counters are zeroed by reset_nj_counters.
   */
+
   uint32_t counter_;
 
   /* Bit used to identify this nested join*/
   std::bitset<64> nj_map;
 
+private:
   /*
     (Valid only for semi-join nests) Bitmap of tables outside the semi-join
     that are used within the semi-join's ON condition.
   */
   table_map sj_depends_on;
   /* Outer non-trivially correlated tables */
+
   table_map sj_corr_tables;
 
   List<Item> sj_outer_expr_list;
@@ -69,10 +87,49 @@ public:
      plan. This means two things.
 
      1. All tables on its @c join_list are covered by the plan.
-
+y
      2. All child join nest nodes are fully covered.
    */
+
+public:
   bool is_fully_covered() const { return join_list.size() == counter_; }
+
+  /* To get the table_map sj_depends_on */
+  table_map getSjDependsOn() const
+  {
+    return sj_depends_on;
+  }
+
+  /* To set the table_map sj_depends_on */
+  void setSjDependsOn(table_map in_sj_depends_on)
+  {
+    sj_depends_on= in_sj_depends_on;
+  }
+
+  /* To get the table_map sj_corr_tables */
+  table_map getSjCorrTables() const
+  {
+    return sj_corr_tables;
+  }
+  
+  /* To set the table_map sj_corr_tables */
+  void getSjCorrTables(table_map in_sj_corr_tables) 
+  {
+    sj_corr_tables= in_sj_corr_tables;
+  }
+
+  /* To get the List sj_outer_expr_list */
+  List<Item> getSjOuterExprList() const
+  {
+    return sj_outer_expr_list;
+  }
+  
+  /* To set the List sj_outer_expr_list */
+  void setSjOuterExprList(List<Item> in_sj_outer_expr_list)
+  {
+    sj_outer_expr_list= in_sj_outer_expr_list;
+  }
+
 };
 
 } /* namespace drizzled */
