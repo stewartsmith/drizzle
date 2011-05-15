0f991b6e9a56e645190518596c33ab0dfced33e1
Daniel Nichter <daniel@percona.com>
2011-05-15 11:58:28 -0600

    Add query_log plugin.  It's tested and documented.

---
+++ plugin/query_log/event.h
@@ -0,0 +1,66 @@
+/*
+  *  Copyright (C) 2011 Daniel Nichter
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#pragma once
+
+/**
+ * @file
+ *   event.h
+ *
+ * @brief
+ *   Defines the event_t struct that encapsulates an event.
+ *
+ * @details
+ *   An event (i.e. a query) has the attributes defined in the event_t struct.
+ *   The values come from various members of the Session class.  This is
+ *   a necessary redundancy for two reasons.  First, access to this data via
+ *   the Session class is not uniform; it requires various calls and
+ *   calculations.  Look at QueryLog::afterStatement() to see this.  Second,
+ *   because the QueryLog object controls the logger classes, i.e.
+ *   QueryLoggerFile and others in the futre, event creation and filtering
+ *   is done in one place (QueryLog::afterStatement()) and then acceptable
+ *   events are passed to the logger classes so that all they have to do is log.
+ *
+ *   Since this is just a collection of variables, making this a class
+ *   with accessor functions is overkill.
+ */
+struct event_t {
+  // GMT timestamps (2002-01-31T10:00:01.123456)
+  std::string ts;
+
+  // integers
+  uint32_t session_id;
+  uint32_t query_id;
+  uint32_t rows_examined;
+  uint32_t rows_sent;
+  uint32_t tmp_tables;
+  uint32_t warnings;
+
+  // times (42.123456)
+  double execution_time;
+  double lock_time;
+  double session_time;
+
+  // bools ("true" or "false")
+  const char *error;
+
+  // strings, not quoted or escaped
+  const char *schema;
+
+  // query, not quoted or escaped
+  const char *query;
+};
---
+++ plugin/query_log/file.cc
@@ -0,0 +1,96 @@
+/* -*- mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; -*-
+ *  vim:expandtab:shiftwidth=2:tabstop=2:smarttab:
+ *
+ *  Copyright (C) 2011 Daniel Nichter
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <config.h>
+#include <string>
+#include <fcntl.h>
+#include "file.h"
+
+using namespace std;
+
+QueryLoggerFile::QueryLoggerFile()
+{
+  _fd= LOG_FILE_CLOSED;
+
+  // If you add something here, the number of params must match the number
+  // of values pushed to the formatter in logEvent().
+  _formatter.parse(
+    "# %s\n"
+    "# session_id=%d query_id=%d rows_examined=%d rows_sent=%d tmp_tables=%d warnings=%d\n"
+    "# execution_time=%.6f lock_time=%.6f session_time=%.6f\n"
+    "# error=%s\n"
+    "# schema=\"%s\"\n"
+    "%s;\n#\n"
+  );
+}
+
+QueryLoggerFile::~QueryLoggerFile()
+{
+  closeLogFile();
+}
+
+bool QueryLoggerFile::logEvent(const event_t *event)
+{
+  if (_fd == LOG_FILE_CLOSED)
+    return false;
+
+  _formatter
+    % event->ts
+    % event->session_id
+    % event->query_id
+    % event->rows_examined
+    % event->rows_sent
+    % event->tmp_tables
+    % event->warnings
+    % event->execution_time
+    % event->lock_time  // broken
+    % event->session_time
+    % event->error
+    % event->schema
+    % event->query;
+  string msgbuf= _formatter.str();
+
+  size_t wrv;
+  wrv= write(_fd, msgbuf.c_str(), msgbuf.length());
+  assert(wrv == msgbuf.length());
+
+  return false; // success
+}
+
+bool QueryLoggerFile::openLogFile(const char *file)
+{
+  assert(file != NULL);
+ 
+  int new_fd= open(file, O_WRONLY | O_APPEND | O_CREAT, S_IRUSR | S_IWUSR);
+  if (new_fd < 0)
+    return true; // error
+
+  closeLogFile();
+  _fd= new_fd;
+
+  return false; // success
+}
+
+bool QueryLoggerFile::closeLogFile()
+{
+  if (not _fd == LOG_FILE_CLOSED)
+    close(_fd);  // TODO: catch errors
+  _fd= LOG_FILE_CLOSED;
+  return false;  // success
+}
