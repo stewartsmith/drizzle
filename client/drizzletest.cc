cb74b7df99136242ca571ee20afe75cf97db8a57
Olaf van der Spek <olafvdspek@gmail.com>
2011-06-19 15:06:54 +0200

    Refactor

--- client/drizzletest.cc
+++ client/drizzletest.cc
@@ -818,49 +818,8 @@ static void handle_command_error(st_command* command, uint32_t error)
   }
 }
 
-
-static void close_files()
-{
-  for (; cur_file >= file_stack.data(); cur_file--)
-  {
-    if (cur_file->file && cur_file->file != stdin)
-      fclose(cur_file->file);
-    free(const_cast<char*>(cur_file->file_name));
-    cur_file->file_name= 0;
-  }
-}
-
-static void free_used_memory()
-{
-  close_files();
-  BOOST_FOREACH(var_hash_t::reference i, var_hash)
-  {
-    free(i.second->str_val);
-    free(i.second->env_s);
-    if (i.second->alloced)
-      free(i.second);
-  }
-  var_hash.clear();
-  BOOST_FOREACH(st_command* i, q_lines)
-    delete i;
-  for (size_t i= 0; i < var_reg.size(); i++)
-  {
-    if (var_reg[i].alloced_len)
-      free(var_reg[i].str_val);
-  }
-  free_all_replace();
-  free(opt_pass);
-}
-
-
 static void cleanup_and_exit(int exit_code)
 {
-  if (0) // Olaf: Freeing resources is unnecessary when exiting
-  {
-    free_used_memory(); 
-    internal::my_end();
-  }
-
   if (!silent) 
   {
     switch (exit_code) 
@@ -1012,12 +971,10 @@ void warning_msg(const char *fmt, ...)
     ds_warning_messages.append("Warning detected ");
     if (cur_file && cur_file != file_stack.data())
     {
-      len= snprintf(buff, sizeof(buff), "in included file %s ",
-                    cur_file->file_name);
+      len= snprintf(buff, sizeof(buff), "in included file %s ", cur_file->file_name);
       ds_warning_messages.append(buff, len);
     }
-    len= snprintf(buff, sizeof(buff), "at line %d: ",
-                  start_lineno);
+    len= snprintf(buff, sizeof(buff), "at line %d: ", start_lineno);
     ds_warning_messages.append(buff, len);
   }
 
