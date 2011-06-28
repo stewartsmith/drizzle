317e078c15a9ee571c6ee0b97b145b7476c03c21
Olaf van der Spek <olafvdspek@gmail.com>
2011-06-28 23:02:08 +0200

    Prepare for PCH

---
+++ drizzled/pch.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <drizzled/common_fwd.h>
+#include <boost/algorithm/string.hpp>
+#include <boost/array.hpp>
+#include <boost/bind.hpp>
+#include <boost/date_time.hpp>
+// #include <boost/filesystem.hpp>
+#include <boost/foreach.hpp>
+#include <boost/program_options.hpp>
+#include <boost/ptr_container/ptr_container.hpp>
+#include <boost/smart_ptr.hpp>
+// #include <boost/thread.hpp>
+#include <boost/unordered_map.hpp>
+#include <boost/unordered_set.hpp>
+#include <boost/utility.hpp>
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
