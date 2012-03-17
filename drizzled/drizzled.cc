703c78eed49ad5313f93c77045950a65b5e14450
Mohit <srivastavamohit91@gmail.com>
2012-03-17 21:43:11 +0530

    Bug:943657 Various error messages like validation error , unknown option error is updated.

--- drizzled/drizzled.cc
+++ drizzled/drizzled.cc
@@ -1276,7 +1276,7 @@ bool init_variables_before_daemonizing(int argc, char **argv)
   }
   catch (po::validation_error &err)
   {
-    unireg_abort << "Use --help to get a list of available options. " << err.what(); 
+    unireg_abort  << err.what() <<".\t"<< "Use --help to get a list of available options. "; 
   }
 
   if (vm.count("version"))
@@ -1316,8 +1316,9 @@ bool init_variables_before_daemonizing(int argc, char **argv)
     po::notify(vm);
   }
   catch (po::validation_error &err)
+
   {
-    unireg_abort << "Use --help to get a list of available options. " << err.what();
+   unireg_abort  << err.what() <<".\t"<< "Use --help to get a list of available options. ";
   }
 
   return true;
@@ -1357,15 +1358,15 @@ bool init_variables_after_daemonizing(module::Registry &plugins)
   }
   catch (po::validation_error &err)
   {
-    unireg_abort << "Use --help to get a list of available options. " << err.what();
+    unireg_abort  << err.what() <<". "<< "Use --help to get a list of available options. ";
   }
   catch (po::invalid_command_line_syntax &err)
   {
-    unireg_abort << "Use --help to get a list of available options. " << err.what();
+    unireg_abort  << err.what() <<". "<< "Use --help to get a list of available options. ";
   }
   catch (po::unknown_option &err)
   {
-    unireg_abort << "Use --help to get a list of available options.  " << err.what();
+    unireg_abort  << err.what() <<". "<< "Use --help to get a list of available options. ";
   }
 
   try
@@ -1374,7 +1375,7 @@ bool init_variables_after_daemonizing(module::Registry &plugins)
   }
   catch (po::validation_error &err)
   {
-    unireg_abort << "Use --help to get a list of available options. " << err.what();
+    unireg_abort  << err.what() <<". "<< "Use --help to get a list of available options. ";
   }
 
   get_options();
