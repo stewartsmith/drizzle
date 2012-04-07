71ac7755c414721aebf063ca74a904c2ebc98c99
Olaf van der Spek <olafvdspek@gmail.com>
2012-04-07 16:12:24 +0200

    Improve find_ptr

--- plugin/user_locks/barriers.cc
+++ plugin/user_locks/barriers.cc
@@ -51,16 +51,16 @@ bool Barriers::create(const user_locks::Key &arg, drizzled::session_id_t owner, 
 return_t Barriers::release(const user_locks::Key &arg, drizzled::session_id_t owner)
 {
   boost::unique_lock<boost::mutex> scope(mutex);
-  Barrier::shared_ptr* iter= find_ptr(barrier_map, arg);
+  Barrier::shared_ptr iter= find_ptr2(barrier_map, arg);
 
   // Nothing is found
   if (not iter)
     return NOT_FOUND;
 
-  if ((*iter)->getOwner() != owner)
+  if (iter->getOwner() != owner)
     return NOT_OWNED_BY;
 
-  (*iter)->signal(); // We tell anyone left to start running
+  iter->signal(); // We tell anyone left to start running
   (void)barrier_map.erase(arg);
 
   return SUCCESS;
@@ -69,10 +69,7 @@ return_t Barriers::release(const user_locks::Key &arg, drizzled::session_id_t ow
 Barrier::shared_ptr Barriers::find(const user_locks::Key &arg)
 {
   boost::unique_lock<boost::mutex> scope(mutex);
-  if (Barrier::shared_ptr* iter= find_ptr(barrier_map, arg))
-    return *iter;
-
-  return Barrier::shared_ptr();
+  return find_ptr2(barrier_map, arg);
 }
 
 void Barriers::Copy(Map &arg)
