5cc3d3a3f17e7cbcbf93d9233ebe7d4e7ea4a9eb
Brian Aker <brian@tangent.org>
2012-10-11 22:27:23 -0700

    Update test case via fix C99 issue

--- libdrizzle-1.0/t/drizzle_st.c
+++ libdrizzle-1.0/t/drizzle_st.c
@@ -59,7 +59,7 @@ int main(void)
 {
   close(STDOUT_FILENO);
 
-  if (getenv("TESTS_ENVIRONMENT") and strstr(getenv("TESTS_ENVIRONMENT"), "valgrind"))
+  if (getenv("TESTS_ENVIRONMENT") && strstr(getenv("TESTS_ENVIRONMENT"), "valgrind"))
   {
     return EXIT_SUCCESS;
   }
